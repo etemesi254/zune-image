98caf610cbf31b03697d2a53455f4cf4c73454f8
caleb <etemesicaleb@gmail.com>
2022-10-01 20:35:38 +0300

    image/errors: Add Error definitions.

--- crates/zune-image/src/errors.rs
+++ crates/zune-image/src/errors.rs
@@ -1,11 +1,29 @@
 use std::fmt::{Debug, Formatter};
 
-pub enum ImgDecodeErrors
+use zune_core::colorspace::ColorSpace;
+
+pub enum ImgErrors
 {
     JpegDecodeErrors(zune_jpeg::errors::DecodeErrors),
+    NoImageForOperations,
+    NoImageBuffer,
+    OperationsError(ImgOperationsErrors),
+    EncodeErrors(ImgEncodeErrors),
 }
 
-impl Debug for ImgDecodeErrors
+pub enum ImgOperationsErrors
+{
+    WrongColorspace(ColorSpace, ColorSpace),
+    WrongComponents(usize, usize),
+}
+
+pub enum ImgEncodeErrors
+{
+    Generic(String),
+    GenericStatic(&'static str),
+}
+
+impl Debug for ImgErrors
 {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result
     {
@@ -15,13 +33,70 @@ impl Debug for ImgDecodeErrors
             {
                 writeln!(f, "Jpeg decoding failed:{:?}", error)
             }
+            Self::NoImageForOperations =>
+            {
+                writeln!(f, "No image found for which we can execute operations")
+            }
+            Self::NoImageBuffer => writeln!(f, "No image buffer present"),
+
+            Self::OperationsError(ref error) => writeln!(f, "{:?}", error),
+
+            Self::EncodeErrors(ref err) => writeln!(f, "{:?}", err),
         }
     }
 }
-impl From<zune_jpeg::errors::DecodeErrors> for ImgDecodeErrors
+impl From<zune_jpeg::errors::DecodeErrors> for ImgErrors
 {
     fn from(from: zune_jpeg::errors::DecodeErrors) -> Self
     {
-        ImgDecodeErrors::JpegDecodeErrors(from)
+        ImgErrors::JpegDecodeErrors(from)
+    }
+}
+
+impl From<ImgOperationsErrors> for ImgErrors
+{
+    fn from(from: ImgOperationsErrors) -> Self
+    {
+        ImgErrors::OperationsError(from)
+    }
+}
+impl From<ImgEncodeErrors> for ImgErrors
+{
+    fn from(from: ImgEncodeErrors) -> Self
+    {
+        ImgErrors::EncodeErrors(from)
+    }
+}
+impl Debug for ImgOperationsErrors
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result
+    {
+        match self
+        {
+            Self::WrongColorspace(ref expected, ref found) =>
+            {
+                writeln!(
+                    f,
+                    "Expected {:?} colorspace but found {:?}",
+                    expected, found
+                )
+            }
+            Self::WrongComponents(expected, found) =>
+            {
+                writeln!(f, "Expected {} components and found {}", expected, found)
+            }
+        }
+    }
+}
+
+impl Debug for ImgEncodeErrors
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result
+    {
+        match self
+        {
+            Self::Generic(ref string) => writeln!(f, "{}", string),
+            Self::GenericStatic(ref string) => writeln!(f, "{}", string),
+        }
     }
 }
