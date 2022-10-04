32d4205485d87f8e5f8ecd9060ad373d51ee739e
caleb <etemesicaleb@gmail.com>
2022-10-04 22:33:34 +0300

    image/impls: Add RGBA and RGBX support for grayscale.

--- crates/zune-image/src/impls/grayscale.rs
+++ crates/zune-image/src/impls/grayscale.rs
@@ -5,6 +5,18 @@ use crate::errors::ImgOperationsErrors;
 use crate::image::{Image, ImageChannels};
 use crate::traits::OperationsTrait;
 
+/// Convert RGB data to grayscale
+///
+/// This will convert any image that contains three
+/// RGB channels(including RGB, RGBA,RGBX) into grayscale
+///
+/// Formula for RGB to grayscale conversion is given by
+///
+/// ```text
+///Grayscale = 0.299R + 0.587G + 0.114B
+/// ```
+/// but it's implemented using fixed point integer mathematics and simd kernels
+/// where applicable (see zune-imageprocs/grayscale)
 pub struct RgbToGrayScale;
 
 impl RgbToGrayScale
@@ -24,7 +36,12 @@ impl OperationsTrait for RgbToGrayScale
 
     fn execute_simple(&self, image: &mut Image) -> Result<(), ImgOperationsErrors>
     {
-        if image.get_colorspace() != ColorSpace::RGB
+        let im_colorspace = image.get_colorspace();
+
+        // Support any colorspace with RGB data
+        if im_colorspace != ColorSpace::RGB
+            || im_colorspace != ColorSpace::RGBA
+            || im_colorspace != ColorSpace::RGBX
         {
             return Err(ImgOperationsErrors::WrongColorspace(
                 ColorSpace::RGB,
@@ -34,11 +51,20 @@ impl OperationsTrait for RgbToGrayScale
 
         let (width, height) = image.get_dimensions();
         let size = width * height;
+
         let mut grayscale = vec![0; size];
 
-        if let ImageChannels::ThreeChannels(rgb_data) = image.get_channel_mut()
+        if let ImageChannels::ThreeChannels(rgb_data) = image.get_channel_ref()
         {
             rgb_to_grayscale((&rgb_data[0], &rgb_data[1], &rgb_data[2]), &mut grayscale);
+        }
+        else if let ImageChannels::FourChannels(rgba_data) = image.get_channel_ref()
+        {
+            // discard alpha channel
+            rgb_to_grayscale(
+                (&rgba_data[0], &rgba_data[1], &rgba_data[2]),
+                &mut grayscale,
+            );
         }
         // change image info to be grayscale
         image.set_image_channel(ImageChannels::OneChannel(grayscale));
