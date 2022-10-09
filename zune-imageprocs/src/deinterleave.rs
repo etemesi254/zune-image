1980549c01960776a5a179fc250d331be18e9417
caleb <etemesicaleb@gmail.com>
2022-10-09 20:08:15 +0300

    imageprocs/deinterleave:  Add imports closer to functions than at the global point.

--- zune-imageprocs/src/deinterleave.rs
+++ zune-imageprocs/src/deinterleave.rs
@@ -1,13 +1,4 @@
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-#[cfg(feature = "avx2")]
-use crate::deinterleave::avx2::de_interleave_3_channels_avx2;
 use crate::deinterleave::scalar::de_interleave_3_channels_scalar;
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-#[cfg(feature = "sse2")]
-use crate::deinterleave::sse2::de_interleave_3_channels_sse2;
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-#[cfg(feature = "sse3")]
-use crate::deinterleave::sse3::de_interleave_3_channels_sse3;
 
 mod avx2;
 mod scalar;
@@ -20,6 +11,8 @@ pub fn de_interleave_3_channels(source: &[u8], (c1, c2, c3): (&mut [u8], &mut [u
     {
         #[cfg(feature = "avx2")]
         {
+            use crate::deinterleave::avx2::de_interleave_3_channels_avx2;
+
             if is_x86_feature_detected!("avx2")
             {
                 unsafe {
@@ -29,6 +22,8 @@ pub fn de_interleave_3_channels(source: &[u8], (c1, c2, c3): (&mut [u8], &mut [u
         }
         #[cfg(feature = "sse3")]
         {
+            use crate::deinterleave::sse3::de_interleave_3_channels_sse3;
+
             if is_x86_feature_detected!("sse3")
             {
                 unsafe {
@@ -38,6 +33,8 @@ pub fn de_interleave_3_channels(source: &[u8], (c1, c2, c3): (&mut [u8], &mut [u
         }
         #[cfg(feature = "sse2")]
         {
+            use crate::deinterleave::sse2::de_interleave_3_channels_sse2;
+
             if is_x86_feature_detected!("sse2")
             {
                 unsafe {
