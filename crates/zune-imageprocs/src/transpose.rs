719a47d148361c1607f3df4ed00399561fcdadab
caleb <etemesicaleb@gmail.com>
2022-10-10 21:49:44 +0300

    imageprocs/transpose: Replace debug with trace

--- crates/zune-imageprocs/src/transpose.rs
+++ crates/zune-imageprocs/src/transpose.rs
@@ -1,4 +1,4 @@
-use log::debug;
+use log::trace;
 
 use crate::transpose::scalar::transpose_scalar;
 
@@ -15,12 +15,12 @@ pub fn transpose(in_matrix: &[u8], out_matrix: &mut [u8], width: usize, height: 
 
             if is_x86_feature_detected!("sse4.1")
             {
-                debug!("Using SSE4.1 transpose algorithm");
+                trace!("Using SSE4.1 transpose algorithm");
                 unsafe { return transpose_sse41(in_matrix, out_matrix, width, height) }
             }
         }
     }
-    debug!("Using scalar transpose algorithm");
+    trace!("Using scalar transpose algorithm");
     transpose_scalar(in_matrix, out_matrix, width, height);
 }
 
