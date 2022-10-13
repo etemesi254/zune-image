30603321ff11845d6ca648978fde91b513722687
caleb <etemesicaleb@gmail.com>
2022-10-13 11:04:16 +0300

    image/impls: Rename DeinterleaveThreeChannels to DeinterleaveChannels.
    The method should encapsulate almost all general purpose deinterleave operations.

--- zune-image/src/impls/deinterleave.rs
+++ zune-image/src/impls/deinterleave.rs
@@ -1,29 +1,31 @@
-use zune_imageprocs::deinterleave::de_interleave_3_channels;
+use zune_imageprocs::deinterleave::{de_interleave_four_channels, de_interleave_three_channels};
 
 use crate::errors::ImgOperationsErrors;
 use crate::image::{Image, ImageChannels};
 use crate::traits::OperationsTrait;
 
 #[derive(Default)]
-pub struct DeInterleaveThreeChannels;
+pub struct DeInterleaveChannels;
 
-impl DeInterleaveThreeChannels
+impl DeInterleaveChannels
 {
-    pub fn new() -> DeInterleaveThreeChannels
+    pub fn new() -> DeInterleaveChannels
     {
-        DeInterleaveThreeChannels {}
+        DeInterleaveChannels {}
     }
 }
-impl OperationsTrait for DeInterleaveThreeChannels
+impl OperationsTrait for DeInterleaveChannels
 {
     fn get_name(&self) -> &'static str
     {
-        "De-interleave 3 channels"
+        "De-interleave channels"
     }
 
     fn execute_simple(&self, image: &mut Image) -> Result<(), ImgOperationsErrors>
     {
         let (width, height) = image.get_dimensions();
+
+        let colorspace = image.get_colorspace();
 
         if image.get_colorspace().num_components() != 3
         {
@@ -35,23 +37,48 @@ impl OperationsTrait for DeInterleaveThreeChannels
         let channel = image.get_channel_mut();
         if let ImageChannels::Interleaved(rgb_channel) = channel
         {
-            // allocate new array based on width and height
-            let size = width * height;
+            // three component de-interleave
+            if colorspace.num_components() == 3
+            {
+                // allocate new array based on width and height
+                let size = width * height;
 
-            assert_eq!(
-                rgb_channel.len(),
-                size * 3,
-                "Length is not equal to dimensions"
-            );
+                if rgb_channel.len() != size * 3
+                {
+                    return Err(ImgOperationsErrors::Generic(
+                        "Image length mismatch, image dimensions do not match array length",
+                    ));
+                }
 
-            let mut c1 = vec![0; size];
-            let mut c2 = vec![0; size];
-            let mut c3 = vec![0; size];
+                let mut c1 = vec![0; size];
+                let mut c2 = vec![0; size];
+                let mut c3 = vec![0; size];
 
-            de_interleave_3_channels(rgb_channel, (&mut c1, &mut c2, &mut c3));
+                de_interleave_three_channels(rgb_channel, (&mut c1, &mut c2, &mut c3));
 
-            // change the channel type to be uninitialized rgb8
-            *channel = ImageChannels::ThreeChannels([c1, c2, c3]);
+                // change the channel type to be uninitialized rgb8
+                *channel = ImageChannels::ThreeChannels([c1, c2, c3]);
+            }
+            else if colorspace.num_components() == 4
+            {
+                // four component deinterleave
+                // allocate new array based on width and height
+                let size = width * height;
+
+                if rgb_channel.len() != size * 4
+                {
+                    return Err(ImgOperationsErrors::Generic(
+                        "Image length mismatch, image dimensions do not match array length",
+                    ));
+                }
+
+                let mut c1 = vec![0; size];
+                let mut c2 = vec![0; size];
+                let mut c3 = vec![0; size];
+                let mut c4 = vec![0; size];
+
+                de_interleave_four_channels(rgb_channel, (&mut c1, &mut c2, &mut c3, &mut c4));
+            }
         }
         Ok(())
     }
