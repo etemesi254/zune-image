bfe25cbfc2376719ef4f64bab2272336e1904ab2
caleb <etemesicaleb@gmail.com>
2022-10-16 13:58:06 +0300

    image/impls: Add mirror operation

---
+++ crates/zune-image/src/impls/mirror.rs
@@ -0,0 +1,73 @@
+use zune_imageprocs::mirror::mirror;
+pub use zune_imageprocs::mirror::MirrorMode;
+
+use crate::errors::ImgOperationsErrors;
+use crate::image::{Image, ImageChannels};
+use crate::traits::OperationsTrait;
+/// Rearrange the pixels up side down
+pub struct Mirror
+{
+    mode: MirrorMode,
+}
+
+impl Mirror
+{
+    pub fn new(mode: MirrorMode) -> Mirror
+    {
+        Self { mode }
+    }
+}
+impl OperationsTrait for Mirror
+{
+    fn get_name(&self) -> &'static str
+    {
+        "Mirror"
+    }
+
+    fn execute_simple(&self, image: &mut Image) -> Result<(), ImgOperationsErrors>
+    {
+        let (width, height) = image.get_dimensions();
+        match image.get_channel_mut()
+        {
+            ImageChannels::OneChannel(input) =>
+            {
+                mirror(input, width, height, self.mode);
+            }
+            ImageChannels::TwoChannels(input) =>
+            {
+                for inp in input
+                {
+                    mirror(inp, width, height, self.mode);
+                }
+            }
+            ImageChannels::ThreeChannels(input) =>
+            {
+                for inp in input
+                {
+                    mirror(inp, width, height, self.mode);
+                }
+            }
+            ImageChannels::FourChannels(input) =>
+            {
+                for inp in input
+                {
+                    mirror(inp, width, height, self.mode);
+                }
+            }
+            ImageChannels::Interleaved(_) =>
+            {
+                return Err(ImgOperationsErrors::InvalidChannelLayout(
+                    "Cannot mirror interleaved pixels \
+                de-interleave the pixels into separate color components first",
+                ));
+            }
+            ImageChannels::Uninitialized =>
+            {
+                return Err(ImgOperationsErrors::InvalidChannelLayout(
+                    "Cannot mirror uninitialized pixels",
+                ))
+            }
+        }
+        Ok(())
+    }
+}
