b8b685c111b06807042e114dedf4da11e8244a2a
caleb <etemesicaleb@gmail.com>
2022-10-16 13:46:15 +0300

    bin/cmd_args: Separate args into sections that make sense.

--- zune-bin/src/cmd_args.rs
+++ zune-bin/src/cmd_args.rs
@@ -1,9 +1,12 @@
-pub(crate) mod jpeg;
-
+pub mod arg_parsers;
+pub mod help_strings;
 use std::ffi::OsString;
 
 use clap::builder::PossibleValue;
 use clap::{value_parser, Arg, ArgAction, ArgGroup, Command, ValueEnum};
+
+use crate::cmd_args::arg_parsers::IColorSpace;
+use crate::cmd_args::help_strings::{BRIGHTEN_HELP, COLORSPACE_HELP, TRANSPOSE_HELP};
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub(crate) enum MmapOptions
@@ -57,8 +60,6 @@ impl CmdOptions
 #[rustfmt::skip]
 pub fn create_cmd_args() -> Command {
     Command::new("zune-images")
-        .subcommand(crate::cmd_args::jpeg::options())
-        .subcommand_precedence_over_arg(true)
         .arg(Arg::new("in")
             .short('i')
             .help("Input file to read data from")
@@ -72,75 +73,134 @@ pub fn create_cmd_args() -> Command {
             .action(ArgAction::Append)
             .value_parser(value_parser!(OsString))
             .required(true))
-        .arg(Arg::new("debug")
-            .long("debug")
-            .action(ArgAction::SetTrue)
-            .help_heading("LOGGING")
-            .help("Display debug information and higher"))
-        .arg(Arg::new("trace")
-            .long("trace")
-            .action(ArgAction::SetTrue)
-            .help_heading("LOGGING")
-            .help("Display very verbose information"))
-        .arg(Arg::new("warn")
-            .long("warn")
-            .action(ArgAction::SetTrue)
-            .help_heading("LOGGING")
-            .help("Display warnings and errors"))
-        .arg(Arg::new("info")
-            .long("info")
-            .action(ArgAction::SetTrue)
-            .help_heading("LOGGING")
-            .help("Display information about the decoding options"))
         .arg(Arg::new("mmap")
             .long("mmap")
             .help_heading("ADVANCED")
             //.takes_value(true)
             .help("Influence the use of memory maps")
             .long_help("Change use of memory maps and how they are used for decoding.\nMemory maps are preferred for large images to keep memory usage low.")
-            .value_parser(value_parser!(MmapOptions))
-        ).arg(Arg::new("all-yes")
+            .value_parser(value_parser!(MmapOptions)))
+        .arg(Arg::new("all-yes")
             .long("yes")
             .help("Answer yes to all queries asked")
-            .action(ArgAction::SetTrue)
-        )
+            .action(ArgAction::SetTrue))
+        .args(add_logging_options())
         .args(add_operations())
+        .args(add_settings())
         .group(ArgGroup::new("operations")
-            .args(["flip","transpose","grayscale","flop"])
+            .args(["flip","transpose","grayscale","flop","mirror","invert","brighten","crop"])
             .multiple(true))
 }
-fn add_operations() -> [Arg; 4]
+
+fn add_logging_options() -> [Arg; 4]
+{
+    [
+        Arg::new("debug")
+            .long("debug")
+            .action(ArgAction::SetTrue)
+            .help_heading("Logging")
+            .help("Display debug information and higher"),
+        Arg::new("trace")
+            .long("trace")
+            .action(ArgAction::SetTrue)
+            .help_heading("Logging")
+            .help("Display very verbose information"),
+        Arg::new("warn")
+            .long("warn")
+            .action(ArgAction::SetTrue)
+            .help_heading("Logging")
+            .help("Display warnings and errors"),
+        Arg::new("info")
+            .long("info")
+            .action(ArgAction::SetTrue)
+            .help_heading("Logging")
+            .help("Display information about the decoding options"),
+    ]
+}
+fn add_settings() -> [Arg; 4]
+{
+    [
+        Arg::new("colorspace")
+            .long("colorspace")
+            .help_heading("Image Settings")
+            .help("Change the image colorspace during decoding")
+            .long_help(COLORSPACE_HELP)
+            .value_parser(value_parser!(IColorSpace))
+            .hide_possible_values(true),
+        Arg::new("max-width")
+            .long("max-width")
+            .help_heading("Image Settings")
+            .help("Maximum width of images allowed")
+            .default_value("37268")
+            .value_parser(value_parser!(usize)),
+        Arg::new("max-height")
+            .long("max-height")
+            .help_heading("Image Settings")
+            .help("Maximum height of images allowed")
+            .default_value("37268")
+            .value_parser(value_parser!(usize)),
+        Arg::new("strict")
+            .long("strict")
+            .help_heading("Image Settings")
+            .help("Treat most warnings as errors")
+            .action(ArgAction::SetTrue)
+            .default_value("false"),
+    ]
+}
+fn add_operations() -> [Arg; 8]
 {
     [
         Arg::new("grayscale")
             .long("grayscale")
-            .help_heading("OPERATIONS")
+            .help_heading("Image Operations")
             .action(ArgAction::SetTrue)
             .help("Convert the image to grayscale")
             .long_help("Change image type from RGB to grayscale")
             .group("operations"),
-
         Arg::new("transpose")
             .long("transpose")
-            .help_heading("OPERATIONS")
+            .help_heading("Image Operations")
             .action(ArgAction::SetTrue)
             .help("Transpose an image")
             .group("operations")
-            .long_help("Transpose an image\nThis mirrors the image along the image top left to bottom-right diagonal"),
-
+            .long_help(TRANSPOSE_HELP),
         Arg::new("flip")
             .long("flip")
-            .help_heading("OPERATIONS")
+            .help_heading("Image Operations")
             .action(ArgAction::SetTrue)
             .help("Flip an image")
             .group("operations"),
-
         Arg::new("flop")
             .long("flop")
-            .help_heading("OPERATIONS")
+            .help_heading("Image Operations")
             .action(ArgAction::SetTrue)
             .help("Flop an image")
-            .group("operations")
+            .group("operations"),
+        Arg::new("mirror")
+            .long("mirror")
+            .help_heading("Image Operations")
+            .value_parser(["north", "south", "east", "west"])
+            .help("Mirror the image")
+            .group("operations"),
+        Arg::new("invert")
+            .long("invert")
+            .help_heading("Image Operations")
+            .action(ArgAction::SetTrue)
+            .help("Invert image pixels")
+            .group("operations"),
+        Arg::new("brighten")
+            .long("brighten")
+            .help_heading("Image Operations")
+            .help("Brighten (or darken) an image.")
+            .long_help(BRIGHTEN_HELP)
+            .allow_negative_numbers(true)
+            .value_parser(value_parser!(i16).range(-255..255))
+            .group("operations"),
+        Arg::new("crop")
+            .long("crop")
+            .help_heading("Image Operations")
+            .help("Crop an image")
+            .group("operations"),
     ]
 }
 
