fdbfcd502a6a70a2ea7356701f444eb7536c3004
caleb <etemesicaleb@gmail.com>
2022-10-16 13:57:53 +0300

    imageprocs/mirror: Add mirror operation

---
+++ crates/zune-imageprocs/src/mirror.rs
@@ -0,0 +1,55 @@
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub enum MirrorMode
+{
+    North,
+    South,
+    East,
+    West,
+}
+pub fn mirror(in_pixels: &mut [u8], width: usize, height: usize, mode: MirrorMode)
+{
+    if mode == MirrorMode::East || mode == MirrorMode::West
+    {
+        for width_stride in in_pixels.chunks_exact_mut(width)
+        {
+            // split into 2
+            let (left, right) = width_stride.split_at_mut(width / 2);
+
+            if mode == MirrorMode::West
+            {
+                // write
+                left.iter().zip(right.iter_mut().rev()).for_each(|(l, r)| {
+                    *r = *l;
+                });
+            }
+            if mode == MirrorMode::East
+            {
+                // write
+                left.iter_mut().zip(right.iter().rev()).for_each(|(l, r)| {
+                    *l = *r;
+                });
+            }
+        }
+    }
+    else if mode == MirrorMode::North || mode == MirrorMode::South
+    {
+        // split the image along the halfway axis
+        let halfway = width * (height / 2);
+
+        let (top, bottom) = in_pixels.split_at_mut(halfway);
+
+        for (top_width_stride, bottom_width_stride) in top
+            .chunks_exact_mut(width)
+            .zip(bottom.rchunks_exact_mut(width))
+        {
+            if mode == MirrorMode::North
+            {
+                bottom_width_stride.copy_from_slice(top_width_stride);
+            }
+            else if mode == MirrorMode::South
+            {
+                top_width_stride.copy_from_slice(bottom_width_stride);
+            }
+        }
+    }
+}
