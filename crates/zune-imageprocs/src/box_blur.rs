61babe4c1339472c6204a82347c6d54cb7acef41
caleb <etemesicaleb@gmail.com>
2022-10-24 19:00:31 +0300

    imageprocs/box_blur: Finalize box blur algorithm.

--- crates/zune-imageprocs/src/box_blur.rs
+++ crates/zune-imageprocs/src/box_blur.rs
@@ -1,8 +1,11 @@
+use log::warn;
+
 use crate::transpose;
 
 fn compute_mod_u32(d: u64) -> u64
 {
-    return (0xFFFF_FFFF_FFFF_FFFF_u64) / d.saturating_add(1);
+    // operator precedence will be the end of me,,
+    return ((0xFFFF_FFFF_FFFF_FFFF_u64) / d) + 1;
 }
 fn mul128_u32(low_bits: u64, d: u32) -> u64
 {
@@ -12,18 +15,19 @@ fn mul128_u32(low_bits: u64, d: u32) -> u64
 #[allow(clippy::cast_possible_truncation)]
 fn fastdiv_u32(a: u32, m: u64) -> u32
 {
-    (mul128_u32(m, a) & u64::from(u32::MAX)) as u32
+    mul128_u32(m, a) as u32
 }
+
 pub fn box_blur(
     in_out_image: &mut [u8], scratch_space: &mut [u8], width: usize, height: usize, radius: usize,
 )
 {
-    if width == 0 || radius == 0
+    if width == 0 || radius <= 1
     {
+        warn!("Box blur with radius less than or equal to 1 does nothing");
         return;
     }
     box_blur_inner(in_out_image, scratch_space, width, height, radius);
-    in_out_image.copy_from_slice(scratch_space);
     transpose::transpose(scratch_space, in_out_image, width, height);
     box_blur_inner(in_out_image, scratch_space, height, width, radius);
     transpose::transpose(scratch_space, in_out_image, height, width);
@@ -31,36 +35,49 @@ pub fn box_blur(
 #[allow(clippy::cast_possible_truncation)]
 fn box_blur_inner(in_image: &[u8], out_image: &mut [u8], width: usize, height: usize, radius: usize)
 {
-    // Box blurs can be seen as the average of three pixels iterating
+    // 1D-Box blurs can be seen as the average of radius pixels iterating
     // through a window
     // A box blur therefore is
     //
     // pix[x,y]= (pix[x-r/2,y]...+pix[x,y]+...pix[x+r/2,y])/r
     //
-    // This is better than the previous one since we don't have to worry
-    // about the addition overflowing and we can use u8s for it
+    // The naive operation is slow, due to a lot of reasons, so here we use a slightly more
+    // optimized version
     //
-    // Another important optimization is that widths are independent of each other
-    // we can use that to our advantage to increase the parallelism
+    // One thing to see is that 1D box blurs are independent per width stride
+    // ie. calculating row y is independent of calculating row y+1, with this info
+    // we can do a bit of loop unrolling to better utilize ILP.
     //
+    // Furthermore, notice we are dividing by r, which is a constant across all rows,
+    // But division is a slow instruction, hence we can replace it with multiplication by some
+    // weird constant, that eliminates that in the inner loop, credits to Daniel Lemire's fastmod for that
+    //
+    // Further more there is no need to sum up a window per iteration, we can simply implement it by looking at what is changing
+    // For any iteration the sum is window[n], and for n+1 sum is window[n+1], but what changed was nothing
+    // i.e what changed was windows[n-r/2] was dropped and windows [n+r/2] was added.
+    // So if we keep the terms window[n-r/2] and windows [n+r/2] the summing becomes
+    // sum(windows[n]) = a - windows[r]-windows[0]
+    // where a is sum of chunk[0..r], (first of the array), we can keep updating a during the loop
+    // and we have a window sum!
 
-    if width == 0 || radius == 0
+    if width <= 1 || radius <= 1
     {
         // repeated here for the optimizer
         return;
     }
     let radius = radius.min(width);
     let m_radius = compute_mod_u32(radius as u64);
-    //
     // Handle data not in boundaries
     for (four_stride_in, four_stride_out) in in_image
         .chunks_exact(width * 4)
         .zip(out_image.chunks_exact_mut(width * 4))
     {
+        // output strides
         let (os1, rem) = four_stride_out.split_at_mut(width);
         let (os2, rem) = rem.split_at_mut(width);
         let (os3, os4) = rem.split_at_mut(width);
 
+        // input strides
         let (ws1, rem) = four_stride_in.split_at(width);
         let (ws2, rem) = rem.split_at(width);
         let (ws3, ws4) = rem.split_at(width);
@@ -87,10 +104,6 @@ fn box_blur_inner(in_image: &[u8], out_image: &mut [u8], width: usize, height: u
             os2[pos] = (a2 / p) as u8;
             os3[pos] = (a3 / p) as u8;
             os4[pos] = (a4 / p) as u8;
-
-            unsafe {
-                std::arch::asm!("");
-            }
         }
 
         let r_window = radius.saturating_add(0);
@@ -134,36 +147,52 @@ fn box_blur_inner(in_image: &[u8], out_image: &mut [u8], width: usize, height: u
         let rows_unhanded = (in_image.len() / width) % 4;
 
         for (in_stride, out_stride) in in_image
-            .rchunks_exact(width)
-            .zip(out_image.rchunks_exact_mut(width))
+            .chunks_exact(width)
+            .zip(out_image.chunks_exact_mut(width))
             .take(rows_unhanded)
         {
             let mut a1 = 0;
 
-            for i in in_stride.iter().take(radius)
+            for (pos, i) in in_stride.iter().take(radius).enumerate()
             {
                 a1 += u32::from(*i);
+                let p = (pos as u32).saturating_add(1);
+                out_stride[pos] = (a1 / p) as u8;
             }
-            let r1 = u32::from(in_stride[0]);
+            let mut r1 = u32::from(in_stride[0]);
 
-            for (w1, o1) in in_stride.windows(radius).zip(out_stride[2..].iter_mut())
+            for (w1, o1) in in_stride
+                .windows(radius)
+                .zip(out_stride[radius..].iter_mut())
             {
                 a1 = a1.wrapping_add(u32::from(w1[radius - 1]).wrapping_sub(r1));
                 *o1 = fastdiv_u32(a1, m_radius) as u8;
+                r1 = u32::from(w1[0]);
             }
         }
     }
 }
 
-#[test]
-fn check_results()
+#[cfg(all(feature = "benchmarks"))]
+#[cfg(test)]
+mod benchmarks
 {
-    let mut t: Vec<u8> = (0..=255).collect();
-    let mut u = vec![0; 256];
-    let v: Vec<u8> = (0..255).collect();
-    box_blur(&mut t, &mut u, 16, 16, 3);
-    for s in t.chunks_exact(16)
+    extern crate test;
+
+    use crate::box_blur::box_blur;
+
+    #[bench]
+    fn bench_box_blur(b: &mut test::Bencher)
     {
-        println!("{:?}", s)
+        let width = 800;
+        let height = 800;
+        let radius = 10;
+        let dimensions = width * height;
+        let mut in_vec = vec![255; dimensions];
+        let mut scratch_space = vec![0; dimensions];
+
+        b.iter(|| {
+            box_blur(&mut in_vec, &mut scratch_space, width, height, radius);
+        });
     }
 }
