649c86867589b0994139ff61000d4261b7c9d8dd
caleb <etemesicaleb@gmail.com>
2022-10-25 07:20:15 +0300

    imageprocs/flop: Improve documentation on flop operation and simplify code.

--- zune-imageprocs/src/flop.rs
+++ zune-imageprocs/src/flop.rs
@@ -1,3 +1,13 @@
+/// Flop an image
+///
+///```text
+///old image     new image
+///┌─────────┐   ┌──────────┐
+///│a b c d e│   │e d b c a │
+///│f g h i j│   │j i h g f │
+///└─────────┘   └──────────┘
+///```
+///
 pub fn flop(in_out_image: &mut [u8], width: usize)
 {
     assert_eq!(
@@ -11,12 +21,10 @@ pub fn flop(in_out_image: &mut [u8], width: usize)
         let (left_to_right, right_to_left) = width_chunks.split_at_mut(width / 2);
 
         // iterate and swap
-        left_to_right
-            .iter_mut()
-            .zip(right_to_left.iter_mut().rev())
-            .for_each(|(ltr, rtl)| {
-                std::mem::swap(ltr, rtl);
-            });
+        for (ltr, rtl) in left_to_right.iter_mut().zip(right_to_left.iter_mut().rev())
+        {
+            std::mem::swap(ltr, rtl);
+        }
     }
 }
 
@@ -26,11 +34,11 @@ mod benchmarks
 {
     extern crate test;
 
-    use crate::flop::flop;
-
     #[bench]
     fn flop_scalar(b: &mut test::Bencher)
     {
+        use crate::flop::flop;
+
         let width = 800;
         let height = 800;
         let dimensions = width * height;
@@ -39,6 +47,6 @@ mod benchmarks
 
         b.iter(|| {
             flop(&mut c1, width);
-        })
+        });
     }
 }
