3b50d270c7c368717188616f75c4b8f938baa0b3
caleb <etemesicaleb@gmail.com>
2022-10-25 07:25:02 +0300

    imageprocs/invert: Improve documentation.

--- crates/zune-imageprocs/src/invert.rs
+++ crates/zune-imageprocs/src/invert.rs
@@ -1,4 +1,8 @@
-pub fn invert(in_matrix: &mut [u8])
+///Invert a pixel
+///
+/// The formula for inverting a 8 bit pixel
+///  is `pixel[x,y] = 255-pixel[x,y]`
+pub fn invert(in_image: &mut [u8])
 {
-    in_matrix.iter_mut().for_each(|x| *x = u8::MAX - *x);
+    in_image.iter_mut().for_each(|x| *x = u8::MAX - *x);
 }
