8802ff4ea80dfd6787295caa7950a5e0eb3af3b1
caleb <etemesicaleb@gmail.com>
2022-11-26 12:14:06 +0300

    imageprocs/utils: Add documentation
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- crates/zune-imageprocs/src/utils.rs
+++ crates/zune-imageprocs/src/utils.rs
@@ -1,4 +1,17 @@
 /// Prefetch data at offset position
+///
+/// This uses prefetch intrinsics for a specific
+/// platform to hint the CPU  that the data at that position
+/// will be needed at a later time.
+///
+/// # Platform specific behaviour
+/// - On x86, we use `_MM_HINT_T0` which prefetches to all levels of cache
+/// hence it may cause cache pollution
+///
+/// # Arguments
+///  - data: A long slice with some data not in the cache
+///  - position: The position of data we expect to fetch that we think
+/// is not in the cache.
 #[inline(always)]
 pub fn z_prefetch<T>(data: &[T], position: usize)
 {
