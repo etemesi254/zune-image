4ff44cb07ff5b7537f76e978fe33534fb5da307e
caleb <etemesicaleb@gmail.com>
2022-11-27 18:09:40 +0300

    core/lib: Add `DecodingResult` enum.
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- zune-core/src/lib.rs
+++ zune-core/src/lib.rs
@@ -2,3 +2,11 @@ pub mod bit_depth;
 #[cfg(feature = "bytestream")]
 pub mod bytestream;
 pub mod colorspace;
+
+/// A simple enum that can hold either
+/// u8's or u16's used for decoding pixels
+pub enum DecodingResult
+{
+    U8(Vec<u8>),
+    U16(Vec<u16>)
+}
