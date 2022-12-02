1b595ac6e0260b4997492c877bbf49ab25a377cd
caleb <etemesicaleb@gmail.com>
2022-12-02 23:19:43 +0300

    png/crc: Silence some warnings
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- zune-png/src/crc/crc32_pclmul.rs
+++ zune-png/src/crc/crc32_pclmul.rs
@@ -1,6 +1,6 @@
-use std::arch::x86_64::{__m128, __m128i, _mm_clmulepi64_si128, _mm_xor_si128};
+use std::arch::x86_64::{__m128i, _mm_clmulepi64_si128, _mm_xor_si128};
 
-unsafe fn fold_sse(src: __m128i, dst: __m128i, multipliers: __m128i) -> __m128i
+unsafe fn _fold_sse(src: __m128i, dst: __m128i, multipliers: __m128i) -> __m128i
 {
     /*
      * The immediate constant for PCLMULQDQ specifies which 64-bit halves of
