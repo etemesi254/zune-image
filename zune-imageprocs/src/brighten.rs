e7acc666ed13bc99bfacc9f296300f0a0495acd7
caleb <etemesicaleb@gmail.com>
2022-12-08 21:34:38 +0300

    imageprocs/brighten: Use saturating adds to ensure pixels don't wrap around.
    Removes green effects when image is brightened too much
    
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- zune-imageprocs/src/brighten.rs
+++ zune-imageprocs/src/brighten.rs
@@ -1,11 +1,11 @@
-use std::ops::Add;
+use crate::traits::NumOps;
 
 #[allow(clippy::cast_sign_loss, clippy::cast_possible_wrap)]
-pub fn brighten<T: Copy + PartialOrd + Add<Output = T> + Ord + Default>(
-    channel: &mut [T], value: T, max_value: T
+pub fn brighten<T: Copy + PartialOrd + NumOps<T> + Ord + Default>(
+    channel: &mut [T], value: T, _max_value: T
 )
 {
     channel
         .iter_mut()
-        .for_each(|x| *x = ((*x).add(value)).clamp(T::default(), max_value));
+        .for_each(|x| *x = (*x).saturating_add(value));
 }
