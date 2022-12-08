656853d299ed682cd4c3a3ed69043c19965fa320
caleb <etemesicaleb@gmail.com>
2022-12-09 00:39:36 +0300

    wasm/enums: Add zune-core mappings to wasm mappings.
    Signed-off-by: caleb <etemesicaleb@gmail.com>

---
+++ zune-wasm/src/enums.rs
@@ -0,0 +1,77 @@
+use wasm_bindgen::prelude::wasm_bindgen;
+use zune_core::colorspace::ColorSpace;
+use zune_image::codecs::SupportedDecoders;
+
+/// A 1 to 1 mapping of supported colorspaces
+/// but with the `wasm_bindgen` attribute.
+#[wasm_bindgen]
+pub enum WasmColorspace
+{
+    RGB,
+    RGBA,
+    YCbCr,
+    Luma,
+    LumaA,
+    RGBX,
+    YCCK,
+    CYMK,
+    Unknown
+}
+
+impl WasmColorspace
+{
+    pub fn from_colorspace(colorspace: ColorSpace) -> WasmColorspace
+    {
+        match colorspace
+        {
+            ColorSpace::RGB => Self::RGB,
+            ColorSpace::RGBA => Self::RGBA,
+            ColorSpace::YCbCr => Self::YCbCr,
+            ColorSpace::Luma => Self::Luma,
+            ColorSpace::LumaA => Self::LumaA,
+            ColorSpace::RGBX => Self::RGBX,
+            ColorSpace::YCCK => Self::YCCK,
+            ColorSpace::CYMK => Self::CYMK,
+            ColorSpace::Unknown => Self::Unknown,
+            _ => panic!("Unknown new colorspace {:?}", colorspace)
+        }
+    }
+}
+
+/// A one-to-one mapping of the image formats currently supported by
+/// the decoder but with a `wasm_bindgen` attribute
+#[wasm_bindgen]
+pub enum WasmImageDecodeFormats
+{
+    /// Fully complete
+    Jpeg,
+    /// Not yet complete
+    Png,
+    /// Fully complete
+    PPM,
+    /// Partial support
+    PSD,
+    /// Full support
+    Farbfeld,
+    /// Full support
+    QOI,
+    /// Any unknown format.
+    Unknown
+}
+
+impl WasmImageDecodeFormats
+{
+    pub fn from_formats(format: SupportedDecoders) -> WasmImageDecodeFormats
+    {
+        match format
+        {
+            SupportedDecoders::Jpeg => Self::Jpeg,
+            SupportedDecoders::Png => Self::Png,
+            SupportedDecoders::PPM => Self::PPM,
+            SupportedDecoders::PSD => Self::PSD,
+            SupportedDecoders::Farbfeld => Self::Farbfeld,
+            SupportedDecoders::QOI => Self::QOI,
+            SupportedDecoders::Unknown => Self::Unknown
+        }
+    }
+}
