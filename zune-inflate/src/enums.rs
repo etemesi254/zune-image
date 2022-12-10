f471c65296101e7ea1d89b9120c89e854e088979
caleb <etemesicaleb@gmail.com>
2022-12-10 19:18:02 +0300

    inflate/enums: Remove enums are we won't be needing them
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- zune-inflate/src/enums.rs
+++
@@ -1,7 +0,0 @@
-#[derive(Debug, Copy, Clone)]
-pub enum DeflateState
-{
-    Initialized,
-    NewBlock,
-    Continue,
-}
--- zune-inflate/src/lib.rs
+++ zune-inflate/src/lib.rs
@@ -4,6 +4,5 @@ pub use crate::decoder::DeflateDecoder;
 mod bitstream;
 mod constants;
 mod decoder;
-mod enums;
 pub mod errors;
 mod utils;
