15a856a5c2ac3e161f997b2b57c49ebc5c4ff07f
caleb <etemesicaleb@gmail.com>
2022-12-12 00:34:43 +0300

    inflate/fuzz: Fix imports
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- crates/zune-inflate/fuzz/fuzz_targets/decode_buffer.rs
+++ crates/zune-inflate/fuzz/fuzz_targets/decode_buffer.rs
@@ -4,6 +4,6 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     // fuzzed code goes here
-    let mut decoder = zune_inflate::ZlibDecoder::new_zlib(data);
+    let mut decoder = zune_inflate::DeflateDecoder::new(data);
     let _ = decoder.decode_zlib();
 });
