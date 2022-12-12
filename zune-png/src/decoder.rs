9ec069d742cc90654f19e5bbb004c97da8f24a19
caleb <etemesicaleb@gmail.com>
2022-12-12 19:28:42 +0300

    png/decoder: Add More decoding code
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- zune-png/src/decoder.rs
+++ zune-png/src/decoder.rs
@@ -1,8 +1,9 @@
-use std::fs::OpenOptions;
-use std::io::Write;
+use std::io::{Cursor, Read};
 
+use zune_core::bit_depth::BitDepth;
 use zune_core::bytestream::ZByteReader;
 use zune_core::colorspace::ColorSpace;
+use zune_core::DecodingResult;
 
 use crate::constants::PNG_SIGNATURE;
 use crate::enums::{FilterMethod, InterlaceMethod, PngChunkType, PngColor};
@@ -37,7 +38,7 @@ pub struct PngDecoder<'a>
     pub(crate) options:     PngOptions,
     pub(crate) png_info:    PngInfo,
     pub(crate) palette:     Vec<u8>,
-    pub(crate) idat_chunks: Vec<&'a [u8]>
+    pub(crate) idat_chunks: Vec<u8>
 }
 
 impl<'a> PngDecoder<'a>
@@ -60,25 +61,42 @@ impl<'a> PngDecoder<'a>
         }
     }
 
-    pub fn get_dimensions(&self) -> Option<(usize, usize)>
+    pub const fn get_dimensions(&self) -> Option<(usize, usize)>
     {
-        if self.seen_hdr
+        if !self.seen_hdr
         {
             return None;
         }
 
         Some((self.png_info.width, self.png_info.height))
     }
-    pub fn get_colorspace(&self) -> ColorSpace
+    pub const fn get_depth(&self) -> Option<BitDepth>
     {
+        if !self.seen_hdr
+        {
+            return None;
+        }
+        match self.png_info.depth
+        {
+            1 | 2 | 4 | 8 => Some(BitDepth::Eight),
+            16 => Some(BitDepth::Sixteen),
+            _ => unreachable!()
+        }
+    }
+    pub fn get_colorspace(&self) -> Option<ColorSpace>
+    {
+        if !self.seen_hdr
+        {
+            return None;
+        }
         match self.png_info.color
         {
-            PngColor::Palette => ColorSpace::RGB,
-            PngColor::Luma => ColorSpace::Luma,
-            PngColor::LumaA => ColorSpace::LumaA,
-            PngColor::RGB => ColorSpace::RGB,
-            PngColor::RGBA => ColorSpace::RGBA,
-            PngColor::Unknown => todo!()
+            PngColor::Palette => Some(ColorSpace::RGB),
+            PngColor::Luma => Some(ColorSpace::Luma),
+            PngColor::LumaA => Some(ColorSpace::LumaA),
+            PngColor::RGB => Some(ColorSpace::RGB),
+            PngColor::RGBA => Some(ColorSpace::RGBA),
+            PngColor::Unknown => unreachable!()
         }
     }
     fn read_chunk_header(&mut self) -> Result<PngChunk, PngErrors>
@@ -155,9 +173,8 @@ impl<'a> PngDecoder<'a>
 
     /// Decode PNG encoded images and return the vector of raw
     /// pixels
-    pub fn decode(&mut self) -> Result<Vec<u8>, PngErrors>
+    pub fn decode(&mut self) -> Result<DecodingResult, PngErrors>
     {
-        let data = vec![];
         // READ PNG signature
         let signature = self.stream.get_u64_be_err()?;
 
@@ -205,11 +222,21 @@ impl<'a> PngDecoder<'a>
             }
         }
         // go parse IDAT chunks
-        self.inflate();
-        Ok(data)
+        let data = self.inflate()?;
+        // now we have uncompressed data from zlib. Undo filtering
+
+        // images with depth of 8, no interlace or filter can proceed to be returned
+        if self.png_info.depth == 8
+            && self.png_info.filter_method == FilterMethod::None
+            && self.png_info.interlace_method == InterlaceMethod::Standard
+        {
+            return Ok(DecodingResult::U8(data));
+        }
+
+        Err(PngErrors::GenericStatic("Not yet done"))
     }
     /// Undo deflate decoding
-    fn inflate(&mut self)
+    fn inflate(&mut self) -> Result<Vec<u8>, PngErrors>
     {
         // An annoying thing is that deflate doesn't
         // store its uncompressed size,
@@ -221,27 +248,65 @@ impl<'a> PngDecoder<'a>
         // the vec, extend and go back to inflate.
         //
         //
-        // so choose point 2.
+        // so choose point 1.
         //
-        // So since we have IDAT chunks as references
-        // we call deflate with those chunks until we have full
-        // data, we can suspend
-        let mut file = OpenOptions::new()
-            .write(true)
-            .open("/home/caleb/Documents/zune-image/zune-inflate/tests/tt.zlib")
-            .unwrap();
+        // This allows the zlib decoder to optimize its own paths(which it does)
+        // because it controls the allocation and doesn't have to check for near EOB
+        // runs.
+        //
 
-        for chunk in &self.idat_chunks
         {
-            file.write_all(chunk).unwrap();
+            use std::fs::OpenOptions;
+            use std::io::Write;
+            let mut file = OpenOptions::new()
+                .write(true)
+                .create(true)
+                .open("/home/caleb/Documents/zune-image/zune-inflate/tests/zlib/41284_PNG.zlib")
+                .unwrap();
+
+            file.write_all(&self.idat_chunks).unwrap();
         }
+        let mut decoder = zune_inflate::DeflateDecoder::new(&self.idat_chunks);
+
+        let uncompressed_data = decoder.decode_zlib().unwrap();
+
+        //let uncompressed_data = _decode_writer_flate(&self.idat_chunks);
+
+        let info = &self.png_info;
+        let img_width_bytes =
+            ((usize::from(info.component) * info.width * usize::from(info.depth)) + 7) >> 3;
+
+        let image_len = (img_width_bytes + 1) * info.height;
+
+        if uncompressed_data.len() < image_len
+        {
+            let msg = format!(
+                "Not enough pixels, expected {} but found {}",
+                image_len,
+                uncompressed_data.len()
+            );
+            return Err(PngErrors::Generic(msg));
+        }
+
+        Ok(uncompressed_data)
     }
+}
+
+fn _decode_writer_flate(bytes: &[u8]) -> Vec<u8>
+{
+    let mut writer = Vec::new();
+
+    let mut deflater = flate2::read::ZlibDecoder::new(Cursor::new(bytes));
+
+    deflater.read_to_end(&mut writer).unwrap();
+
+    writer
 }
 
 #[test]
 fn decode()
 {
-    let data = std::fs::read("/home/caleb/jpeg/cballs2.png").unwrap();
+    let data = std::fs::read("/home/caleb/jpeg/mahasahiro.png").unwrap();
     let mut decoder = PngDecoder::new(&data);
     decoder.decode().unwrap();
 }
