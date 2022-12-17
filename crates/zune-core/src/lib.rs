4117c97aeb8eaeef9d070fbaac26c22fae3f937a
caleb <etemesicaleb@gmail.com>
2022-12-17 18:23:09 +0300

    core: Add `u8` method on `DecodingResult`
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- crates/zune-core/src/lib.rs
+++ crates/zune-core/src/lib.rs
@@ -9,3 +9,33 @@ pub enum DecodingResult
     U8(Vec<u8>),
     U16(Vec<u16>)
 }
+
+impl DecodingResult
+{
+    /// Return the contents if the enum stores Vec<u8> or otherwise
+    /// return None.
+    ///
+    /// Useful for de-sugaring the result of a decoding operation
+    /// into row bytes
+    ///
+    /// # Example
+    /// ```
+    /// use zune_core::DecodingResult;
+    /// let data = DecodingResult::U8(vec![0;100]);
+    /// // we know this won't fail because we created it with u8
+    /// assert!(data.u8().is_some());
+    ///
+    /// let data = DecodingResult::U16(vec![0;100]);
+    /// // it should now return nothing since the type is u18
+    /// assert!(data.u8().is_none());
+    ///
+    /// ```
+    pub fn u8(self) -> Option<Vec<u8>>
+    {
+        match self
+        {
+            DecodingResult::U8(data) => Some(data),
+            _ => None
+        }
+    }
+}
