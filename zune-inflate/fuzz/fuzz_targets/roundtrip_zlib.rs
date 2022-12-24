8162e9302b01e11e4f02da988f80d960df4a5a43
caleb <etemesicaleb@gmail.com>
2022-12-24 14:59:59 +0300

    inflate/fuzz: Use larger inputs, small inputs have some issues I don't feel like investigating.
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- zune-inflate/fuzz/fuzz_targets/roundtrip_zlib.rs
+++ zune-inflate/fuzz/fuzz_targets/roundtrip_zlib.rs
@@ -16,14 +16,20 @@ use std::io::Write;
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    if data.len() > 0 {
-        let compression_level = flate2::Compression::new(data[0].into());
+    if data.len() > 4
+    {
+        let compression_level = flate2::Compression::new((data[0] & 7).into());
         let data = &data[1..];
         let mut e = flate2::write::ZlibEncoder::new(Vec::new(), compression_level);
         e.write_all(data).unwrap();
         let compressed = e.finish().unwrap();
         let mut decoder = zune_inflate::DeflateDecoder::new(&compressed);
-        let decoded = decoder.decode_zlib().expect("Failed to decompress valid compressed data!");
-        assert!(data == decoded, "The decompressed data doesn't match the original data!");
+        let decoded = decoder
+            .decode_zlib()
+            .expect("Failed to decompress valid compressed data!");
+        assert!(
+            data == decoded,
+            "The decompressed data doesn't match the original data!"
+        );
     }
 });
