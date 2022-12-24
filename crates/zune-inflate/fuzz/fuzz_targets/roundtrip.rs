596761d66c3c893a6540bd4dc125fbc692f93b5b
caleb <etemesicaleb@gmail.com>
2022-12-24 14:59:59 +0300

    inflate/fuzz: Use larger inputs, small inputs have some issues I don't feel like investigating.
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- crates/zune-inflate/fuzz/fuzz_targets/roundtrip.rs
+++ crates/zune-inflate/fuzz/fuzz_targets/roundtrip.rs
@@ -3,12 +3,18 @@
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    if data.len() > 0 {
+    if data.len() > 1
+    {
         let compression_level = data[0];
         let data = &data[1..];
         let compressed = miniz_oxide::deflate::compress_to_vec(data, compression_level);
         let mut decoder = zune_inflate::DeflateDecoder::new(&compressed);
-        let decoded = decoder.decode_deflate().expect("Failed to decompress valid compressed data!");
-        assert!(data == decoded, "The decompressed data doesn't match the original data!");
+        let decoded = decoder
+            .decode_deflate()
+            .expect("Failed to decompress valid compressed data!");
+        assert!(
+            data == decoded,
+            "The decompressed data doesn't match the original data!"
+        );
     }
 });
