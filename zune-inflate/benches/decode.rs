b826f4dbaa3b37710a6fac0139e9d0818044a1ef
caleb <etemesicaleb@gmail.com>
2022-12-26 23:38:54 +0300

    inflate/benches/decode: Add libdeflate decoder.
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- zune-inflate/benches/decode.rs
+++ zune-inflate/benches/decode.rs
@@ -22,21 +22,35 @@ fn decode_writer_zune(bytes: &[u8]) -> Vec<u8>
     deflater.decode_zlib().unwrap()
 }
 
+fn decode_writer_libdeflate(bytes: &[u8]) -> Vec<u8>
+{
+    let mut deflater = libdeflater::Decompressor::new();
+    // decompressed size is 43 mb. so allocate 50 mb
+    let mut out = vec![0; (1 << 20) * 50];
+
+    deflater.zlib_decompress(bytes, &mut out).unwrap();
+    out
+}
+
 fn decode_test(c: &mut Criterion)
 {
-    let path = env!("CARGO_MANIFEST_DIR").to_string() + "/tests/zlib/tt.zlib";
+    let path = env!("CARGO_MANIFEST_DIR").to_string() + "/tests/zlib/enwiki_part.zlib";
 
     let data = read(path).unwrap();
 
     let mut group = c.benchmark_group("ZLIB decoding");
     group.throughput(Throughput::Bytes(data.len() as u64));
 
-    // group.bench_function("FLATE-[ZLIB-NG]", |b| {
-    //     b.iter(|| black_box(decode_writer_flate(data.as_slice())))
-    // });
+    group.bench_function("FLATE-[miniz-oxide]", |b| {
+        b.iter(|| black_box(decode_writer_flate(data.as_slice())))
+    });
 
     group.bench_function("ZUNE", |b| {
         b.iter(|| black_box(decode_writer_zune(data.as_slice())))
+    });
+
+    group.bench_function("libdeflate", |b| {
+        b.iter(|| black_box(decode_writer_libdeflate(data.as_slice())))
     });
 }
 criterion_group!(name=benches;
