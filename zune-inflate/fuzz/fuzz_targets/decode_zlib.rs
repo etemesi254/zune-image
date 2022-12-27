785f9ae0a6a31727e3cd69a49db966a6fc883e84
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2022-12-27 07:10:05 +0100

    Add a DEFLATE fuzzing target, without zlib

--- zune-inflate/fuzz/fuzz_targets/decode_zlib.rs
+++ zune-inflate/fuzz/fuzz_targets/decode_zlib.rs
@@ -4,5 +4,5 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     let mut decoder = zune_inflate::DeflateDecoder::new(data);
-    let _result = decoder.decode_deflate();
+    let _result = decoder.decode_zlib();
 });
