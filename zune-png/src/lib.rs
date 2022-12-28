045980945db460152b0c72fe0fa0377318807aab
caleb <etemesicaleb@gmail.com>
2022-12-29 01:30:04 +0300

    inflate/benches: Add Interlaced benchmarks.
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- zune-png/src/lib.rs
+++ zune-png/src/lib.rs
@@ -1,3 +1,25 @@
+//!A png decoder
+//!
+//! # Using
+//!
+//! Below is an example of how to decode images
+//! ```no_run
+//! use zune_png::PngDecoder;
+//! use zune_core::DecodingResult;
+//! let mut decoder = PngDecoder::new(&[]);
+//!
+//! let pixels = decoder.decode().unwrap();
+//! let value=match pixels {
+//!
+//!     DecodingResult::U8(value)=>{
+//!         // you got  a u8
+//!     }
+//!     DecodingResult::U16(value)=>{
+//!         // you got a u16, squeeze it to u8?
+//!     }
+//! };
+//! ```
+//!
 pub use decoder::PngDecoder;
 
 mod constants;
