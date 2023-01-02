d05ced5663469559774ebc851990d09319d5925a
caleb <etemesicaleb@gmail.com>
2023-01-02 18:40:24 +0300

    jpeg/fuzz: Fix fuzzing error
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- crates/zune-jpeg/fuzz/fuzz_targets/decode_buffer.rs
+++ crates/zune-jpeg/fuzz/fuzz_targets/decode_buffer.rs
@@ -1,7 +1,8 @@
 #![no_main]
+
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     let mut decoder = zune_jpeg::JpegDecoder::new(data);
-    let _ = decoder.decode_buffer();
+    let _ = decoder.decode();
 });
