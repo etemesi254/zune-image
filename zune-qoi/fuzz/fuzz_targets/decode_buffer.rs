cc34152b91e612b2c2ac40d474198c8ebf244cd6
caleb <etemesicaleb@gmail.com>
2023-01-03 15:33:11 +0300

    qoi: Add fuzzing support
    Signed-off-by: caleb <etemesicaleb@gmail.com>

---
+++ zune-qoi/fuzz/fuzz_targets/decode_buffer.rs
@@ -0,0 +1,10 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: &[u8]| {
+    // fuzzed code goes here
+
+    let mut decoder = zune_qoi::QoiDecoder::new(data);
+    let _ = decoder.decode();
+});
