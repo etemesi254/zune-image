385f974bf5019e6de7b41995438dc1a587d17fbc
caleb <etemesicaleb@gmail.com>
2023-01-11 11:25:23 +0300

    core/options: Added encoder options.
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- crates/zune-core/src/options.rs
+++ crates/zune-core/src/options.rs
@@ -1,12 +1,13 @@
-//! Decoder options
+//! Decoder and Encoder Options
 //!
 //! This module exposes a struct for which all implemented
 //! decoders get shared options for decoding
 //!
-//! All supported options are put into one DecoderOptions to allow for global configurations
-//! options i.e the same  `DecoderOption` can be reused for all other decoders
+//! All supported options are put into one _Options to allow for global configurations
+//! options e.g the same  `DecoderOption` can be reused for all other decoders
 //!
 
+use crate::bit_depth::BitDepth;
 use crate::colorspace::ColorSpace;
 
 /// Decoder options
@@ -94,6 +95,72 @@ impl Default for DecoderOptions
             use_unsafe:     true,
             max_scans:      100,
             strict_mode:    false
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct EncoderOptions
+{
+    /// The image width
+    ///
+    /// This is the width the image will be encoded in
+    ///
+    ///
+    /// # Note
+    /// Images have different width and height limit,
+    /// encoding an image larger than that limit is an error.
+    ///
+    /// Consult with your favourite image codec on its limit
+    pub width:      usize,
+    /// The image height
+    ///
+    /// This is the height the image will be encoded in.
+    ///
+    /// # Note
+    /// Images have different width and height limit,
+    /// encoding an image larger than that limit is an error.
+    ///
+    /// Consult with your favourite image codec on its limit
+    pub height:     usize,
+    /// The colorspace the pixels are in.
+    ///
+    /// # Note
+    /// Each encoder has different set of supported colorspaces.
+    ///
+    /// Check with your favourite image codec on its limit.
+    pub colorspace: ColorSpace,
+    /// The quality expected for the image
+    ///
+    /// This has different results depending on encoder.
+    ///
+    /// - lossy encoders: Higher values, good visual quality/larger file, lower values bad visual quality/smaller file.
+    /// - lossless encoders: Higher values, more encoding time/smaller image, lower value, less encoding time/image.
+    ///
+    ///
+    /// Though this is not respected by some encoders, e.g `ppm` doesn't have a notion of quality.
+    pub quality:    u8,
+    /// The bit depth of the data
+    ///
+    ///
+    /// The data is expected in native endian
+    /// the encoder will convert the data to whatever endian
+    /// is needed by the format.
+    ///
+    /// - Respected by: `png`,`ppm`
+    pub depth:      BitDepth
+}
+
+impl Default for EncoderOptions
+{
+    fn default() -> Self
+    {
+        Self {
+            width:      0,
+            height:     0,
+            colorspace: ColorSpace::RGB,
+            quality:    100,
+            depth:      BitDepth::Eight
         }
     }
 }
