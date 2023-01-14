0eee7243f8158eee39cbe477f76cf4741f98a60e
caleb <etemesicaleb@gmail.com>
2023-01-14 23:48:53 +0300

    core/lib: Add simple documentation
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- crates/zune-core/src/lib.rs
+++ crates/zune-core/src/lib.rs
@@ -1,3 +1,8 @@
+//! Core routines shared by all libraries
+//!
+//! This crate provides a set of core routines shared
+//! by the decoders and encoders under `zune`
+//!
 pub mod bit_depth;
 pub mod bytestream;
 pub mod colorspace;
