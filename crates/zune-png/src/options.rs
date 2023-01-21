4e04784256e365670517d764f67c689e05facbd9
caleb <etemesicaleb@gmail.com>
2023-01-21 18:44:53 +0300

    png/options: Remove  `PngOptions`
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- crates/zune-png/src/options.rs
+++ crates/zune-png/src/options.rs
@@ -6,12 +6,12 @@ use crate::error::PngErrors;
 ///
 pub type UnkownChunkHandler = fn(
     length: usize,
-    chunkType: [u8; 4],
+    chunk_type: [u8; 4],
     reader: &mut ZByteReader,
     crc: u32
 ) -> Result<(), PngErrors>;
 
-fn default_chunk_handler(
+pub fn default_chunk_handler(
     length: usize, chunk_type: [u8; 4], reader: &mut ZByteReader, _crc: u32
 ) -> Result<(), PngErrors>
 {
@@ -31,27 +31,4 @@ fn default_chunk_handler(
     reader.skip(length + 4);
 
     Ok(())
-}
-#[derive(Copy, Clone)]
-pub struct PngOptions
-{
-    pub(crate) max_width:         usize,
-    pub(crate) max_height:        usize,
-    pub(crate) chunk_handler:     UnkownChunkHandler,
-    pub(crate) strict_mode:       bool,
-    pub(crate) confirm_checksums: bool
-}
-
-impl Default for PngOptions
-{
-    fn default() -> Self
-    {
-        Self {
-            max_width:         1 << 17,
-            max_height:        1 << 17,
-            chunk_handler:     default_chunk_handler,
-            strict_mode:       false,
-            confirm_checksums: true
-        }
-    }
 }
