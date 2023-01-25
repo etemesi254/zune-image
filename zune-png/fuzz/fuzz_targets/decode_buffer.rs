f40d7749e9800f46a44b38ae2ed16ca71266ef4a
caleb <etemesicaleb@gmail.com>
2023-01-25 10:45:27 +0300

    png/fuzz/decode_buffer: Don't confirm checksums when fuzzing
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- zune-png/fuzz/fuzz_targets/decode_buffer.rs
+++ zune-png/fuzz/fuzz_targets/decode_buffer.rs
@@ -4,5 +4,6 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     let mut decoder = zune_png::PngDecoder::new(data);
+    decoder.confirm_checksums(false);
     let _ = decoder.decode();
 });
