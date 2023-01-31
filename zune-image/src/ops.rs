6010d4bc8f28f01fcdc3b06bda5b398d9b9c14b0
caleb <etemesicaleb@gmail.com>
2023-01-31 15:42:11 +0300

    image/ops: Add overloadable operations
    Signed-off-by: caleb <etemesicaleb@gmail.com>

---
+++ zune-image/src/ops.rs
@@ -0,0 +1,140 @@
+//! Overloadable operators
+//!
+//! This module provides overloadable operators for the image struct
+//!
+//! The operations panic in case of the incompatibility between two images
+//! so it is best you ensure the image struct is the same
+//!
+use core::ops::*;
+
+use zune_core::bit_depth::BitType;
+
+use crate::image::Image;
+
+#[track_caller]
+fn check_full_compatibility(img1: &Image, img2: &Image)
+{
+    assert_eq!(
+        img1.get_depth(),
+        img2.get_depth(),
+        "Image depth mismatch cannot continue {:?}!={:?}",
+        img1.get_depth(),
+        img2.get_depth()
+    );
+
+    assert_eq!(
+        img1.get_dimensions(),
+        img2.get_dimensions(),
+        "Dimensions mismatch, {:?} != {:?}",
+        img1.get_dimensions(),
+        img2.get_dimensions()
+    );
+
+    assert_eq!(
+        img1.get_colorspace(),
+        img2.get_colorspace(),
+        "Colorspace mismatch, {:?} != {:?}",
+        img1.get_colorspace(),
+        img2.get_colorspace()
+    );
+}
+
+impl Add for Image
+{
+    type Output = Image;
+
+    fn add(self, rhs: Image) -> Self::Output
+    {
+        check_full_compatibility(&self, &rhs);
+
+        let mut new_img = self.clone();
+
+        match self.get_depth().bit_type()
+        {
+            BitType::Eight =>
+            {
+                for (prev, rhs_c) in new_img
+                    .get_channels_mut(true)
+                    .iter_mut()
+                    .zip(rhs.get_channels_ref(true))
+                {
+                    let channel_px = prev.reinterpret_as_mut::<u8>().unwrap();
+                    let channel_rhs = rhs_c.reinterpret_as::<u8>().unwrap();
+
+                    channel_px
+                        .iter_mut()
+                        .zip(channel_rhs.iter())
+                        .for_each(|(x, y)| *x = x.saturating_add(*y));
+                }
+            }
+            BitType::Sixteen =>
+            {
+                for (prev, rhs_c) in new_img
+                    .get_channels_mut(true)
+                    .iter_mut()
+                    .zip(rhs.get_channels_ref(true))
+                {
+                    let channel_px = prev.reinterpret_as_mut::<u16>().unwrap();
+                    let channel_rhs = rhs_c.reinterpret_as::<u16>().unwrap();
+
+                    channel_px
+                        .iter_mut()
+                        .zip(channel_rhs.iter())
+                        .for_each(|(x, y)| *x = x.saturating_add(*y));
+                }
+            }
+            _ => todo!()
+        }
+        new_img
+    }
+}
+
+impl Sub for Image
+{
+    type Output = Image;
+
+    fn sub(self, rhs: Image) -> Self::Output
+    {
+        check_full_compatibility(&self, &rhs);
+
+        let mut new_img = self.clone();
+
+        match self.get_depth().bit_type()
+        {
+            BitType::Eight =>
+            {
+                for (prev, rhs_c) in new_img
+                    .get_channels_mut(true)
+                    .iter_mut()
+                    .zip(rhs.get_channels_ref(true))
+                {
+                    let channel_px = prev.reinterpret_as_mut::<u8>().unwrap();
+                    let channel_rhs = rhs_c.reinterpret_as::<u8>().unwrap();
+
+                    channel_px
+                        .iter_mut()
+                        .zip(channel_rhs.iter())
+                        .for_each(|(x, y)| *x = x.saturating_sub(*y));
+                }
+            }
+            BitType::Sixteen =>
+            {
+                for (prev, rhs_c) in new_img
+                    .get_channels_mut(true)
+                    .iter_mut()
+                    .zip(rhs.get_channels_ref(true))
+                {
+                    let channel_px = prev.reinterpret_as_mut::<u16>().unwrap();
+                    let channel_rhs = rhs_c.reinterpret_as::<u16>().unwrap();
+
+                    channel_px
+                        .iter_mut()
+                        .zip(channel_rhs.iter())
+                        .for_each(|(x, y)| *x = x.saturating_sub(*y));
+                }
+            }
+            _ => todo!()
+        }
+        new_img
+    }
+}
