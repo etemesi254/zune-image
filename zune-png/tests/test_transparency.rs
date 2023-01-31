3a3d285b5bf47b6ea697514306cd01e0ff72fc84
caleb <etemesicaleb@gmail.com>
2023-01-31 11:02:47 +0300

    png/tests: Add transaprency test for 16 bit images
    Signed-off-by: caleb <etemesicaleb@gmail.com>

---
+++ zune-png/tests/test_transparency.rs
@@ -0,0 +1,47 @@
+use std::fs::read;
+use std::path::Path;
+
+use png::Transformations;
+
+fn open_and_read<P: AsRef<Path>>(path: P) -> Vec<u8>
+{
+    read(path).unwrap()
+}
+
+fn decode_ref(data: &[u8]) -> Vec<u8>
+{
+    let mut decoder = png::Decoder::new(data);
+    let expand = Transformations::EXPAND;
+    decoder.set_transformations(expand);
+
+    let mut reader = decoder.read_info().unwrap();
+
+    // Allocate the output buffer.
+    let mut buf = vec![0; reader.output_buffer_size()];
+    // Read the next frame. An APNG might contain multiple frames.
+    let _ = reader.next_frame(&mut buf).unwrap();
+
+    buf
+}
+
+fn decode_zune(data: &[u8]) -> Vec<u8>
+{
+    zune_png::PngDecoder::new(data).decode_raw().unwrap()
+}
+
+fn test_decoding<P: AsRef<Path>>(path: P)
+{
+    let contents = open_and_read(path);
+
+    let zune_results = decode_zune(&contents);
+    let ref_results = decode_ref(&contents);
+    assert_eq!(&zune_results, &ref_results);
+}
+
+#[test]
+fn test_palette_trns_16bit()
+{
+    let path = env!("CARGO_MANIFEST_DIR").to_string() + "/tests/png_suite/tbwn0g16.png";
+
+    test_decoding(path);
+}
