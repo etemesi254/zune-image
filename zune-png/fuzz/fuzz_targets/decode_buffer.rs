008f02ac24464870e0b18e5172a4db5cd59e0bb8
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2023-02-01 20:59:00 +0100

    Fix up the decode_buffer fuzz target

--- zune-png/fuzz/fuzz_targets/decode_buffer.rs
+++ zune-png/fuzz/fuzz_targets/decode_buffer.rs
@@ -4,6 +4,6 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     let opts = zune_core::options::DecoderOptions::new_fast();
-    let mut decoder = zune_png::PngDecoder::new_with_options(data, options);
+    let mut decoder = zune_png::PngDecoder::new_with_options(data, opts);
     let _ = decoder.decode();
 });
