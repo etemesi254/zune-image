b9be92011c521d8fe2cda1fb815d555e55f6b6e5
caleb <etemesicaleb@gmail.com>
2023-02-15 15:41:20 +0300

    core: Add serde support for traits
    Signed-off-by: caleb <etemesicaleb@gmail.com>

---
+++ zune-core/src/serde.rs
@@ -0,0 +1,40 @@
+#![cfg(feature = "serde")]
+
+use serde::ser::*;
+
+use crate::bit_depth::BitDepth;
+use crate::colorspace::{ColorCharacteristics, ColorSpace};
+
+impl Serialize for ColorSpace
+{
+    #[allow(clippy::uninlined_format_args)]
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer
+    {
+        // colorspace serialization is simply it's debug value
+        serializer.serialize_str(&format!("{:?}", self))
+    }
+}
+
+impl Serialize for BitDepth
+{
+    #[allow(clippy::uninlined_format_args)]
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer
+    {
+        serializer.serialize_str(&format!("{:?}", self))
+    }
+}
+
+impl Serialize for ColorCharacteristics
+{
+    #[allow(clippy::uninlined_format_args)]
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer
+    {
+        serializer.serialize_str(&format!("{:?}", self))
+    }
+}
