5ae19034861565f5487cabca382aa3beafdacda1
caleb <etemesicaleb@gmail.com>
2023-02-16 12:37:12 +0300

    bin/probe_files: Add comments on mmap and options
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- zune-bin/src/probe_files.rs
+++ zune-bin/src/probe_files.rs
@@ -22,6 +22,8 @@ pub fn probe_input_files(args: &ArgMatches)
                 {
                     let file = File::open(in_file).unwrap();
                     let file_size = file.metadata().unwrap().len();
+                    // Unsafety: Mmap in Linux is not protected, interesting things
+                    // will occur if you mess with the file
                     let mmap = unsafe { Mmap::map(&file).unwrap() };
 
                     let file_contents = mmap.deref();
@@ -29,9 +31,12 @@ pub fn probe_input_files(args: &ArgMatches)
                     if let Some(format) =
                         zune_image::codecs::ImageFormat::guess_format(file_contents)
                     {
+                        // set to high to remove restrictions.
+                        // We'll just be reading headers so it doesn't matter
                         let options = DecoderOptions::new_cmd()
                             .set_max_height(usize::MAX)
                             .set_max_width(usize::MAX);
+
                         let mut decoder = format.get_decoder_with_options(file_contents, options);
 
                         if let Ok(Some(metadata)) = decoder.read_headers()
