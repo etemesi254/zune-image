fe9b9817f8292798c04f078032a4f7126fe9ffac
caleb <etemesicaleb@gmail.com>
2023-03-07 21:24:04 +0300

    tests: Add PSD tests and separate modules for testing
    Signed-off-by: caleb <etemesicaleb@gmail.com>

---
+++ crates/zune-tests/src/jpeg.rs
@@ -0,0 +1,64 @@
+use std::fs::read;
+use std::path::{Path, PathBuf};
+
+use zune_core::options::DecoderOptions;
+use zune_jpeg::JpegDecoder;
+
+use crate::{hash, sample_path, TestEntry};
+
+pub fn jpeg_path() -> PathBuf
+{
+    sample_path().join("test-images/jpeg")
+}
+
+#[test]
+#[allow(clippy::uninlined_format_args)]
+fn test_jpeg()
+{
+    let file = Path::new(env!("CARGO_MANIFEST_DIR")).join("tests/jpeg.json");
+
+    let json_file = read(file).unwrap();
+
+    let paths: Vec<TestEntry> = serde_json::from_slice(&json_file).unwrap();
+
+    let default_path = jpeg_path();
+    let mut error = false;
+    let mut files = Vec::new();
+    for path in &paths
+    {
+        let file_name = default_path.join(&path.name);
+
+        let expected_hash = path.hash;
+
+        // load file
+        let file_contents = read(&file_name).unwrap();
+
+        let mut options = DecoderOptions::default();
+
+        if let Some(color) = path.colorspace
+        {
+            options = options.jpeg_set_out_colorspace(color.to_colorspace());
+        }
+
+        let mut decoder = JpegDecoder::new_with_options(options, &file_contents);
+        let pixels = decoder.decode().unwrap();
+
+        let hash = hash(&pixels);
+
+        if hash != expected_hash
+        {
+            error = true;
+            files.push(path.to_owned());
+            // report error
+            let err = format!(
+                "Hash mismatch for file {:?}\nExpected {} but found {}\nConfig:{:#?}",
+                file_name, expected_hash, hash, path
+            );
+            eprintln!("{}\n", err)
+        }
+    }
+    if error
+    {
+        panic!("Errors found during test decoding\n {:#?}", files);
+    }
+}
--- crates/zune-tests/src/lib.rs
+++ crates/zune-tests/src/lib.rs
@@ -1,117 +1,61 @@
-use std::fs::{read, read_dir, OpenOptions};
-use std::io::Write;
+#![allow(unused_imports)]
+
+use std::fs::read;
 use std::path::{Path, PathBuf};
 
-use serde::*;
+use serde::Deserialize;
+use xxhash_rust::xxh3::xxh3_128;
+use zune_core::colorspace::ColorSpace;
+use zune_core::options::DecoderOptions;
 
-/// A random seed constant,
-///
-/// Has no entropy importance, just an important number
-/// to me
-const SEED: u64 = 37;
+mod jpeg;
+mod psd;
 
-#[derive(Deserialize, Serialize, Clone, Debug)]
-pub struct TestEntry
+#[derive(Copy, Clone, Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum JsonColorspace
 {
-    file: String,
-    hash: u64
+    RGB,
+    RGBA,
+    CMYK,
+    YCbCr,
+    Luma,
+    YCCK
 }
 
-/// Calculate the hash of bytes using SEED as a seed
-/// constant
-fn calculate_hash(bytes: &[u8]) -> u128
+impl JsonColorspace
 {
-    xxhash_rust::xxh3::xxh3_128_with_seed(bytes, SEED)
-}
-
-pub fn create_hash(file: String, bytes: &[u8]) -> TestEntry
-{
-    let hash = (calculate_hash(bytes) >> 64) as u64;
-
-    TestEntry { file, hash }
-}
-
-/// Create a list of test entries from a directory by reading all files
-/// in the directory and creating  a  hash from them
-///
-/// # Arguments
-/// - main_dir: CARGO_ENV_DIR
-/// - extra: Path relative to the repo
-///
-/// - filter: A function that when called returns true or false indicating whether the file
-/// should be decoded.
-/// E.g can be used to accept only some file types in a directory with mixed entries.
-///  On true, the file is decoded, on false the file is skipped
-///
-/// - decoder: The actual decoder implementation, the decoder should override `decode` trait to get
-/// and returns pixels from the image.
-/// Those pixels will be hashed by using xxh3 and the 128 bits will be stored
-///
-/// # Returns.
-/// - The hashes of all passed files in the directory.
-///
-/// # Panics
-/// - A lot
-pub fn create_tests_from_dirs<T, F>(
-    main_dir: String, extra: String, filter: F, decoder: &mut T
-) -> Vec<TestEntry>
-where
-    T: TestTrait,
-    F: Fn(&PathBuf) -> bool
-{
-    // read directory we were passed
-    let directories = read_dir(&(main_dir.clone() + &extra)).unwrap();
-
-    let mut hashes = Vec::with_capacity(21);
-
-    for directory in directories.flatten()
+    pub fn to_colorspace(self) -> ColorSpace
     {
-        if directory.path().is_file() && filter(&directory.path())
-        // ignore nested directories.
+        match self
         {
-            dbg!(directory.path());
-            // read file
-            let file_contents = read(directory.path()).unwrap();
-            let pixels = decoder.decode(&file_contents);
-            let file_path = directory
-                .path()
-                .to_str()
-                .unwrap()
-                .to_string()
-                .replace(&main_dir, "");
-
-            let test = create_hash(file_path, &pixels);
-
-            hashes.push(test);
+            Self::CMYK => ColorSpace::CMYK,
+            Self::YCCK => ColorSpace::YCCK,
+            Self::RGB => ColorSpace::RGB,
+            Self::YCbCr => ColorSpace::YCbCr,
+            Self::Luma => ColorSpace::Luma,
+            Self::RGBA => ColorSpace::RGBA
         }
     }
-    hashes
 }
 
-pub trait TestTrait
+#[derive(Clone, Deserialize, Debug)]
+pub struct TestEntry
 {
-    ///
-    fn decode(&mut self, compressed_bytes: &[u8]) -> Vec<u8>;
+    pub name:       String,
+    pub hash:       u128,
+    pub colorspace: Option<JsonColorspace>,
+    pub comment:    Option<String>
 }
 
-#[derive(Serialize, Deserialize)]
-struct Tests
+pub fn sample_path() -> PathBuf
 {
-    file: Vec<TestEntry>
+    let path = Path::new(env!("CARGO_MANIFEST_DIR"));
+    // get parent path
+    path.parent().unwrap().to_owned()
 }
 
-pub fn write_to_file<P: AsRef<Path>>(file: P, tests: &[TestEntry])
+fn hash(contents: &[u8]) -> u128
 {
-    let t = Tests {
-        file: tests.to_vec()
-    };
-    let string = toml::to_vec(&t).unwrap();
-
-    OpenOptions::new()
-        .write(true)
-        .create(true)
-        .open(file)
-        .unwrap()
-        .write_all(&string)
-        .unwrap();
+    xxh3_128(contents)
 }
