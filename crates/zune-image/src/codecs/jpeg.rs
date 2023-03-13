d73e68938f2249dac4195e6d41af68d435ebdb01
caleb <etemesicaleb@gmail.com>
2023-03-13 15:55:38 +0300

    image: Add support for jpeg encoding.
    This uses `jpeg-encoder` crate at https://crates.io/crates/jpeg-encoder which is a really nice encoder.
    
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- crates/zune-image/src/codecs/jpeg.rs
+++ crates/zune-image/src/codecs/jpeg.rs
@@ -1,4 +1,4 @@
-#![cfg(feature = "zune-jpeg")]
+#![cfg(feature = "jpeg")]
 //! This represents a jpeg decoder instance
 //!
 //!
@@ -6,6 +6,8 @@
 //! and implements `DecoderTrait` for the library
 //!
 //!
+use jpeg_encoder::{ColorType, EncodingError, JpegColorType};
+use log::info;
 use zune_core::bit_depth::BitDepth;
 use zune_core::colorspace::ColorSpace;
 use zune_jpeg::errors::DecodeErrors;
@@ -14,10 +16,11 @@ pub use zune_jpeg::{ImageInfo, JpegDecoder};
 
 use crate::codecs::ImageFormat;
 use crate::deinterleave::deinterleave_u8;
-use crate::errors::ImgErrors;
+use crate::errors::{ImgEncodeErrors, ImgErrors};
 use crate::image::Image;
+use crate::impls::depth::Depth;
 use crate::metadata::ImageMetadata;
-use crate::traits::DecoderTrait;
+use crate::traits::{DecoderTrait, EncoderTrait, OperationsTrait};
 
 impl<'a> DecoderTrait<'a> for zune_jpeg::JpegDecoder<'a>
 {
@@ -80,5 +83,141 @@ impl From<zune_jpeg::errors::DecodeErrors> for ImgErrors
         let err = format!("jpg: {from:?}");
 
         ImgErrors::ImageDecodeErrors(err)
+    }
+}
+// Okay I just need to really appreciate jpeg-encoder crate
+// it's well written and covers quite a lot of things really
+// well, so thank you Volker Ströbel
+
+/// A simple JPEG encoder
+#[derive(Copy, Clone, Default)]
+pub struct JpegEncoder
+{
+    quality:           u8,
+    progressive:       bool,
+    optimized_huffman: bool
+}
+
+impl JpegEncoder
+{
+    /// Create a new decoder with a specified quality
+    pub fn new(quality: u8) -> JpegEncoder
+    {
+        JpegEncoder {
+            quality:           quality,
+            progressive:       false,
+            optimized_huffman: true
+        }
+    }
+}
+
+impl EncoderTrait for JpegEncoder
+{
+    fn get_name(&self) -> &'static str
+    {
+        "jpeg-encoder(vstroebel)"
+    }
+
+    fn encode_inner(&mut self, image: &Image) -> Result<Vec<u8>, ImgEncodeErrors>
+    {
+        let pixels = {
+            if image.get_depth() != BitDepth::Eight
+            {
+                info!("Image is not 8 bit, jpeg expects an 8 bit image, converting it from {:?} bit to 8 bit",image.get_depth());
+                // clone the image
+                // and convert it from u16 to u8
+                let mut new_clone = image.clone();
+                let new_depth = Depth::new(BitDepth::Eight);
+                // convert to 8bit
+                new_depth
+                    .execute(&mut new_clone)
+                    .expect("Could not convert image to bit-depth of 8");
+                // then flatten
+                new_clone.flatten::<u8>()
+            }
+            else
+            {
+                // expected depth, just flatten
+                image.flatten::<u8>()
+            }
+        };
+        if let Some(colorspace) = match_colorspace_to_colortype(image.get_colorspace())
+        {
+            let max_dims = usize::from(u16::MAX);
+            let (width, height) = image.get_dimensions();
+
+            // check dimensions
+            if (width > max_dims) || (height > max_dims)
+            {
+                let msg = format!(
+                    "Too large image dimensions {} x {}, maximum is {} x {}",
+                    width, height, max_dims, max_dims
+                );
+                return Err(ImgEncodeErrors::ImageEncodeErrors(msg));
+            }
+            // create space for our encoder
+            let mut encoded_data =
+                Vec::with_capacity(width * height * image.get_colorspace().num_components());
+            // create encoder finally
+            // vec<u8> supports write so we use that as our encoder
+            let mut encoder = jpeg_encoder::Encoder::new(&mut encoded_data, self.quality);
+
+            // add options
+            encoder.set_progressive(self.progressive);
+            encoder.set_optimized_huffman_tables(self.optimized_huffman);
+
+            info!("Calling jpeg external encoder,(jpeg-encoder)");
+            encoder.encode(&pixels, width as u16, height as u16, colorspace)?;
+
+            Ok(encoded_data)
+        }
+        else
+        {
+            return Err(ImgEncodeErrors::UnsupportedColorspace(
+                image.get_colorspace(),
+                self.supported_colorspaces()
+            ));
+        }
+    }
+
+    fn supported_colorspaces(&self) -> &'static [ColorSpace]
+    {
+        // should match with the
+        // jpeg-encoder crate
+        &[
+            ColorSpace::Luma,
+            ColorSpace::RGB,
+            ColorSpace::RGBA,
+            ColorSpace::YCbCr,
+            ColorSpace::YCCK
+        ]
+    }
+
+    fn format(&self) -> ImageFormat
+    {
+        ImageFormat::JPEG
+    }
+}
+
+/// Match the library colorspace to jpeg color type
+const fn match_colorspace_to_colortype(colorspace: ColorSpace) -> Option<ColorType>
+{
+    match colorspace
+    {
+        ColorSpace::RGBA => Some(ColorType::Rgba),
+        ColorSpace::RGB => Some(ColorType::Rgb),
+        ColorSpace::YCbCr => Some(ColorType::Ycbcr),
+        ColorSpace::Luma => Some(ColorType::Luma),
+        ColorSpace::YCCK => Some(ColorType::Ycck),
+        ColorSpace::CMYK => Some(ColorType::Cmyk),
+        _ => None
+    }
+}
+
+impl From<EncodingError> for ImgEncodeErrors
+{
+    fn from(value: EncodingError) -> Self
+    {
+        ImgEncodeErrors::Generic(value.to_string())
     }
 }
