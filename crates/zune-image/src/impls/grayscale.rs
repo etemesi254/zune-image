ff2c21d1e2a24aea31b6f62a6475a574b018bf6a
caleb <etemesicaleb@gmail.com>
2023-03-24 14:03:37 +0300

    image/impls: Fix implementations that would not work with new frame architecture
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- crates/zune-image/src/impls/grayscale.rs
+++ crates/zune-image/src/impls/grayscale.rs
@@ -60,68 +60,78 @@ impl OperationsTrait for RgbToGrayScale
         let (width, height) = image.get_dimensions();
         let size = width * height * image.get_depth().size_of();
 
-        let channel = image.get_channels_ref(self.preserve_alpha);
+        let colorspace = image.get_colorspace();
+        let depth = image.get_depth();
+        let max_value = image.get_depth().max_value();
 
-        match image.get_depth().bit_type()
+        let mut out_colorspace = ColorSpace::Unknown;
+
+        for frame in image.get_frames_mut()
         {
-            BitType::U8 =>
+            let channel = frame.get_channels_ref(colorspace, self.preserve_alpha);
+
+            match depth.bit_type()
             {
-                let r = channel[0].reinterpret_as::<u8>().unwrap();
-                let g = channel[1].reinterpret_as::<u8>().unwrap();
-                let b = channel[2].reinterpret_as::<u8>().unwrap();
-                let mut out = Channel::new_with_length::<u8>(size);
-
-                rgb_to_grayscale_u8(
-                    r,
-                    g,
-                    b,
-                    out.reinterpret_as_mut::<u8>().unwrap(),
-                    image.get_depth().max_value() as u8
-                );
-
-                if self.preserve_alpha && image.get_colorspace().has_alpha()
+                BitType::U8 =>
                 {
-                    image.set_channels(vec![out, channel[3].clone()]);
-                    image.set_colorspace(ColorSpace::LumaA);
-                }
-                else
-                {
-                    image.set_channels(vec![out]);
-                    image.set_colorspace(ColorSpace::Luma);
-                }
+                    let r = channel[0].reinterpret_as::<u8>().unwrap();
+                    let g = channel[1].reinterpret_as::<u8>().unwrap();
+                    let b = channel[2].reinterpret_as::<u8>().unwrap();
+                    let mut out = Channel::new_with_length::<u8>(size);
 
-                Ok(())
+                    rgb_to_grayscale_u8(
+                        r,
+                        g,
+                        b,
+                        out.reinterpret_as_mut::<u8>().unwrap(),
+                        max_value as u8
+                    );
+
+                    if self.preserve_alpha && colorspace.has_alpha()
+                    {
+                        frame.set_channels(vec![out, channel[3].clone()]);
+                        out_colorspace = ColorSpace::LumaA;
+                    }
+                    else
+                    {
+                        frame.set_channels(vec![out]);
+                        out_colorspace = ColorSpace::Luma;
+                    }
+                }
+                BitType::U16 =>
+                {
+                    let r = channel[0].reinterpret_as::<u16>().unwrap();
+                    let g = channel[1].reinterpret_as::<u16>().unwrap();
+                    let b = channel[2].reinterpret_as::<u16>().unwrap();
+                    let mut out = Channel::new_with_length::<u16>(size);
+
+                    rgb_to_grayscale_u16(
+                        r,
+                        g,
+                        b,
+                        out.reinterpret_as_mut::<u16>().unwrap(),
+                        max_value
+                    );
+
+                    if self.preserve_alpha && colorspace.has_alpha()
+                    {
+                        frame.set_channels(vec![out, channel[3].clone()]);
+                        out_colorspace = ColorSpace::LumaA;
+                    }
+                    else
+                    {
+                        frame.set_channels(vec![out]);
+                        out_colorspace = ColorSpace::Luma;
+                    }
+                }
+                _ => todo!()
             }
-            BitType::U16 =>
-            {
-                let r = channel[0].reinterpret_as::<u16>().unwrap();
-                let g = channel[1].reinterpret_as::<u16>().unwrap();
-                let b = channel[2].reinterpret_as::<u16>().unwrap();
-                let mut out = Channel::new_with_length::<u16>(size);
-
-                rgb_to_grayscale_u16(
-                    r,
-                    g,
-                    b,
-                    out.reinterpret_as_mut::<u16>().unwrap(),
-                    image.get_depth().max_value()
-                );
-
-                if self.preserve_alpha && image.get_colorspace().has_alpha()
-                {
-                    image.set_channels(vec![out, channel[3].clone()]);
-                    image.set_colorspace(ColorSpace::LumaA);
-                }
-                else
-                {
-                    image.set_channels(vec![out]);
-                    image.set_colorspace(ColorSpace::Luma);
-                }
-
-                Ok(())
-            }
-            _ => todo!()
         }
+
+        assert_ne!(out_colorspace, ColorSpace::Unknown);
+
+        image.set_colorspace(out_colorspace);
+        Ok(())
     }
 
     fn supported_colorspaces(&self) -> &'static [ColorSpace]
