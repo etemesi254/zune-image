8d289c9d9cf366108afb01d4a866674294035fcf
caleb <etemesicaleb@gmail.com>
2023-03-26 14:15:14 +0300

    image/examples: Add example that matches image-rs generate_fractals.rs
    Signed-off-by: caleb <etemesicaleb@gmail.com>

---
+++ zune-image/examples/generate_fractals.rs
@@ -0,0 +1,52 @@
+//! This example generates the same fractals present in the
+//! image-rs examples
+//!
+//! It shows use of `from_fn` methods and `modify_pixels_mut` methods
+
+fn main()
+{
+    use zune_core::colorspace::ColorSpace;
+    use zune_image::codecs::jpeg::JpegEncoder;
+    use zune_image::traits::EncoderTrait;
+
+    let img_x = 800;
+    let img_y = 800;
+
+    let scale_x = 3.0 / img_x as f32;
+    let scale_y = 3.0 / img_y as f32;
+
+    let mut image = zune_image::image::Image::from_fn(img_x, img_y, ColorSpace::RGB, |y, x, px| {
+        let r = (0.3 * x as f32) as u8;
+        let b = (0.3 * y as f32) as u8;
+
+        px[0] = r;
+        px[1] = 0;
+        px[2] = b;
+    });
+
+    // This may actually be combined with the function `from_fn` above.
+    // But it tries to match the example given as much as possible
+    image
+        .modify_pixels_mut::<u8, _>(|y, x, px| {
+            let cx = y as f32 * scale_x - 1.5;
+            let cy = x as f32 * scale_y - 1.5;
+
+            let c = num_complex::Complex::new(-0.4, 0.6);
+            let mut z = num_complex::Complex::new(cx, cy);
+
+            let mut i = 0;
+            while i < 255 && z.norm() <= 2.0
+            {
+                z = z * z + c;
+                i += 1;
+            }
+            // write it
+            *px[1] = i as u8;
+        })
+        .unwrap();
+
+    let mut encoder = JpegEncoder::new(80);
+    let px = encoder.encode(&image).unwrap();
+
+    std::fs::write("./fractals.jpg", px).unwrap();
+}
