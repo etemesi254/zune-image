a5315c842efd2d640d9afdb4b4fc56051da06a65
caleb <etemesicaleb@gmail.com>
2023-03-26 14:35:28 +0300

    image/tests: Initial commit
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- crates/zune-image/src/lib.rs
+++ crates/zune-image/src/lib.rs
@@ -1,3 +1,21 @@
+//! A fast and simple image processing library
+//!
+//! This ties up most of the independent crates in
+//!
+//!
+//!
+//! # Things the library is not good at.
+//!
+//! - Per pixel access methods, while there are methods provided for
+//! such things such as [from_fn](crate::image::Image::from_fn) and
+//! [modify_pixels_mut](crate::image::Image::modify_pixels_mut) the images
+//! are represented as planar hence there will be a lot of cache misses as opposed
+//! to others that represents pixels as one single continuous buffer.
+//!
+//! if you plan on doing multiple per pixel manipulations, [image] crate may be a better
+//! fit for your needs
+//!
+//![image]:https://crates.io/crates/image
 #![allow(clippy::redundant_field_names, clippy::uninlined_format_args)]
 extern crate core;
 
@@ -11,5 +29,6 @@ pub mod impls;
 pub mod metadata;
 pub mod ops;
 mod serde;
+mod tests;
 pub mod traits;
 pub mod workflow;
