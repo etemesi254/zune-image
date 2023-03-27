cb1f302a5934c2749adaf291d446e2c1b8a72a33
caleb <etemesicaleb@gmail.com>
2023-03-27 12:01:12 +0300

    image/deinterleave: Add de-interleaving for f32's
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- zune-image/src/deinterleave.rs
+++ zune-image/src/deinterleave.rs
@@ -11,7 +11,8 @@
 //! post processing for scenarios where processing is slow.
 use zune_core::colorspace::ColorSpace;
 use zune_imageprocs::deinterleave::{
-    de_interleave_four_channels_u16, de_interleave_three_channels_u16,
+    de_interleave_four_channels_f32, de_interleave_four_channels_u16,
+    de_interleave_three_channels_f32, de_interleave_three_channels_u16,
     de_interleave_three_channels_u8, deinterleave_four_channels_u8
 };
 
@@ -161,5 +162,76 @@ pub fn deinterleave_u16(
         return Ok(vec![c1, c2, c3, c4]);
     }
 
+    todo!()
+}
+
+pub fn deinterleave_f32(
+    interleaved_pixels: &[f32], colorspace: ColorSpace
+) -> Result<Vec<Channel>, ImgOperationsErrors>
+{
+    if interleaved_pixels.len() % colorspace.num_components() != 0
+    {
+        return Err(ImgOperationsErrors::InvalidChannelLayout(
+            "Extra pixels in the colorspace"
+        ));
+    }
+    let size = (interleaved_pixels.len() / colorspace.num_components()) * 4 /*Depth 4  bytes*/;
+
+    if colorspace.num_components() == 1
+    {
+        let mut c1 = Channel::new_with_capacity::<f32>(size);
+
+        c1.extend(interleaved_pixels);
+
+        return Ok(vec![c1]);
+    }
+    else if colorspace.num_components() == 2
+    {
+        let mut c1 = Channel::new_with_length::<f32>(size);
+        let mut c2 = Channel::new_with_length::<f32>(size);
+
+        let c1_mut = c1.reinterpret_as_mut::<f32>().unwrap();
+        let c2_mut = c2.reinterpret_as_mut::<f32>().unwrap();
+
+        for ((chunk, c1), c2) in interleaved_pixels.chunks_exact(2).zip(c1_mut).zip(c2_mut)
+        {
+            *c1 = chunk[0];
+            *c2 = chunk[1];
+        }
+
+        return Ok(vec![c1, c2]);
+    }
+    // three component de-interleave
+    else if colorspace.num_components() == 3
+    {
+        let mut c1 = Channel::new_with_length::<f32>(size);
+        let mut c2 = Channel::new_with_length::<f32>(size);
+        let mut c3 = Channel::new_with_length::<f32>(size);
+
+        let c1_mut = c1.reinterpret_as_mut::<f32>().unwrap();
+        let c2_mut = c2.reinterpret_as_mut::<f32>().unwrap();
+        let c3_mut = c3.reinterpret_as_mut::<f32>().unwrap();
+
+        de_interleave_three_channels_f32(interleaved_pixels, c1_mut, c2_mut, c3_mut);
+
+        // change the channel type to be uninitialized rgb8
+        return Ok(vec![c1, c2, c3]);
+    }
+    else if colorspace.num_components() == 4
+    {
+        let mut c1 = Channel::new_with_length::<f32>(size);
+        let mut c2 = Channel::new_with_length::<f32>(size);
+        let mut c3 = Channel::new_with_length::<f32>(size);
+        let mut c4 = Channel::new_with_length::<f32>(size);
+
+        let c1_mut = c1.reinterpret_as_mut::<f32>().unwrap();
+        let c2_mut = c2.reinterpret_as_mut::<f32>().unwrap();
+        let c3_mut = c3.reinterpret_as_mut::<f32>().unwrap();
+        let c4_mut = c4.reinterpret_as_mut::<f32>().unwrap();
+
+        de_interleave_four_channels_f32(interleaved_pixels, c1_mut, c2_mut, c3_mut, c4_mut);
+
+        return Ok(vec![c1, c2, c3, c4]);
+    }
     todo!()
 }
