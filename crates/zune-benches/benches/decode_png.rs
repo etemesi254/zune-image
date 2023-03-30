21dbadcb3ed5539be1b78826faf4fab772ffa4ed
caleb <etemesicaleb@gmail.com>
2023-03-31 01:16:42 +0300

    benches: Update png requirements
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- crates/zune-benches/benches/decode_png.rs
+++ crates/zune-benches/benches/decode_png.rs
@@ -2,11 +2,14 @@ use std::fs::read;
 use std::time::Duration;
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
+use spng::DecodeFlags;
 use zune_benches::sample_path;
 
 fn decode_ref(data: &[u8]) -> Vec<u8>
 {
-    let decoder = png::Decoder::new(data);
+    let mut decoder = png::Decoder::new(data);
+    decoder.set_transformations(png::Transformations::EXPAND);
+
     let mut reader = decoder.read_info().unwrap();
 
     // Allocate the output buffer.
@@ -25,7 +28,8 @@ fn decode_zune(data: &[u8]) -> Vec<u8>
 fn decode_spng(data: &[u8]) -> Vec<u8>
 {
     let cursor = std::io::Cursor::new(data);
-    let decoder = spng::Decoder::new(cursor);
+    let mut decoder = spng::Decoder::new(cursor);
+    decoder.set_decode_flags(DecodeFlags::TRANSPARENCY);
     let (_, mut reader) = decoder.read_info().unwrap();
     let output_buffer_size = reader.output_buffer_size();
     let mut out = vec![0; output_buffer_size];
@@ -113,11 +117,38 @@ fn decode_test_16_bit(c: &mut Criterion)
         b.iter(|| black_box(decode_lodepng(data.as_slice())))
     });
 }
+
+fn decode_test_trns_chunk(c: &mut Criterion)
+{
+    let path = sample_path().join("test-images/png/benchmarks/trns.png");
+    let data = read(path).unwrap();
+
+    let mut group = c.benchmark_group("png: PNG decoding  trns image");
+    group.throughput(Throughput::Bytes(data.len() as u64));
+
+    group.bench_function("zune-png", |b| {
+        b.iter(|| black_box(decode_zune(data.as_slice())))
+    });
+
+    group.bench_function("image-rs/png", |b| {
+        b.iter(|| black_box(decode_ref(data.as_slice())))
+    });
+
+    group.bench_function("spng", |b| {
+        b.iter(|| black_box(decode_spng(data.as_slice())))
+    });
+
+    group.bench_function("lodepng", |b| {
+        b.iter(|| black_box(decode_lodepng(data.as_slice())))
+    });
+}
 criterion_group!(name=benches;
-      config={
-      let c = Criterion::default();
-        c.measurement_time(Duration::from_secs(20))
-      };
-    targets=decode_test_16_bit,decode_test,decode_test_interlaced);
+  config={
+  let c = Criterion::default();
+    c.measurement_time(Duration::from_secs(20))
+  };
+targets=decode_test_trns_chunk
+//targets=decode_test_16_bit,decode_test,decode_test_interlaced
+);
 
 criterion_main!(benches);
