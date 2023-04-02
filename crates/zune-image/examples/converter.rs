089f2257a40c16c3e76e2cf82dba876d37f85805
caleb <etemesicaleb@gmail.com>
2023-04-02 19:29:41 +0300

    image/examples: Added a simple image converter
    Signed-off-by: caleb <etemesicaleb@gmail.com>

---
+++ crates/zune-image/examples/converter.rs
@@ -0,0 +1,38 @@
+//! Shows how you can write a simple image converter using
+//! zune library.
+//!
+//! Any supported format will work with this, the library will
+//! automatically encode it for you
+//!
+//!
+//! Shows usage of the `Image::open` and `Image::save` functions
+use zune_image::image::Image;
+
+fn main()
+{
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 3
+    {
+        println!("format {:?} [input] [output]", args[0]);
+        return;
+    }
+    match Image::open(&args[1])
+    {
+        Ok(image) => match image.save(&args[2])
+        {
+            Err(e) =>
+            {
+                eprintln!("An error occurred encoding image {e:?}")
+            }
+            Ok(()) =>
+            {
+                eprintln!("Successfully encoded  {} to {}", &args[1], &args[2])
+            }
+        },
+        Err(e) =>
+        {
+            eprintln!("An error occurred decoding image {e:?}")
+        }
+    }
+}
