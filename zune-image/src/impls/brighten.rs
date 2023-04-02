b692d1ab58445cb43d93244e316c8616465a9636
caleb <etemesicaleb@gmail.com>
2023-04-02 19:00:00 +0300

    image/impls/brighten: Add support for f32 brighten operations
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- zune-image/src/impls/brighten.rs
+++ zune-image/src/impls/brighten.rs
@@ -1,6 +1,6 @@
 use zune_core::bit_depth::BitType;
 use zune_core::colorspace::ColorSpace;
-use zune_imageprocs::brighten::brighten;
+use zune_imageprocs::brighten::{brighten, brighten_f32};
 
 use crate::errors::ImageErrors;
 use crate::image::Image;
@@ -9,12 +9,12 @@ use crate::traits::OperationsTrait;
 #[derive(Default)]
 pub struct Brighten
 {
-    value: i16
+    value: f32
 }
 
 impl Brighten
 {
-    pub fn new(value: i16) -> Brighten
+    pub fn new(value: f32) -> Brighten
     {
         Brighten { value }
     }
@@ -45,6 +45,11 @@ impl OperationsTrait for Brighten
                     self.value as u16,
                     max_val
                 ),
+                BitType::F32 => brighten_f32(
+                    channel.reinterpret_as_mut::<f32>().unwrap(),
+                    self.value,
+                    max_val as f32
+                ),
                 _ => todo!()
             }
         }
@@ -62,6 +67,6 @@ impl OperationsTrait for Brighten
 
     fn supported_types(&self) -> &'static [BitType]
     {
-        &[BitType::U8, BitType::U16]
+        &[BitType::U8, BitType::U16, BitType::F32]
     }
 }
--- zune-imageprocs/src/brighten.rs
+++ zune-imageprocs/src/brighten.rs
@@ -1,11 +1,18 @@
 use crate::traits::NumOps;
 
 #[allow(clippy::cast_sign_loss, clippy::cast_possible_wrap)]
-pub fn brighten<T: Copy + PartialOrd + NumOps<T> + Ord + Default>(
+pub fn brighten<T: Copy + PartialOrd + NumOps<T> + Default>(
     channel: &mut [T], value: T, _max_value: T
 )
 {
     channel
         .iter_mut()
         .for_each(|x| *x = (*x).saturating_add(value));
+}
+
+pub fn brighten_f32(channel: &mut [f32], value: f32, max_value: f32)
+{
+    channel
+        .iter_mut()
+        .for_each(|x| *x = (*x + value).clamp(0.0, max_value));
 }
