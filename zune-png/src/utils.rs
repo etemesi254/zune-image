90e31a73ded03f9e7e78645dcedd83de439fccd6
caleb <etemesicaleb@gmail.com>
2023-04-09 12:42:12 +0300

    zune: Formatting changes
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- zune-png/src/utils.rs
+++ zune-png/src/utils.rs
@@ -1,7 +1,9 @@
 use crate::decoder::PLTEEntry;
 use crate::enums::PngColor;
 
-pub(crate) fn expand_palette(input: &[u8], out: &mut [u8], palette: &[PLTEEntry; 256], components: usize)
+pub(crate) fn expand_palette(
+    input: &[u8], out: &mut [u8], palette: &[PLTEEntry; 256], components: usize
+)
 {
     if components == 0
     {
@@ -158,28 +160,30 @@ pub fn expand_trns<const SIXTEEN_BITS: bool>(
 
 /// Expand bits to bytes expand images with less than 8 bpp
 pub(crate) fn expand_bits_to_byte(
-    width: usize, depth: usize, out_n: usize, plte_present: bool,
-    input: &[u8], out: &mut [u8]
+    width: usize, depth: usize, out_n: usize, plte_present: bool, input: &[u8], out: &mut [u8]
 )
 {
-    let scale = if plte_present {
+    let scale = if plte_present
+    {
         // When a palette is used we only separate the indexes in this pass,
         // the palette pass will convert indexes to the right colors later.
         1
-    } else {
-        match depth {
+    }
+    else
+    {
+        match depth
+        {
             1 => 0xFF,
             2 => 0x55,
             4 => 0x11,
-            _ => return,
+            _ => return
         }
     };
-    
+
     let out = &mut out[..width * out_n];
 
     if depth == 1
     {
-
         let mut in_iter = input.iter();
         let mut out_iter = out.chunks_exact_mut(8);
 
@@ -189,22 +193,25 @@ pub(crate) fn expand_bits_to_byte(
         // The ordering of the iterators is important:
         // `out_iter` must come before `in_iter` so that `in_iter` is not advanced
         // when `out_iter` is less than 8 bytes long
-        (&mut out_iter).zip(&mut in_iter).for_each(|(out_vals, in_val)| {
-            // make sure we only perform the bounds check once
-            let cur: &mut [u8; 8] = out_vals.try_into().unwrap();
-            // perform the actual expansion
-            cur[0] = scale * ((in_val >> 7) & 0x01);
-            cur[1] = scale * ((in_val >> 6) & 0x01);
-            cur[2] = scale * ((in_val >> 5) & 0x01);
-            cur[3] = scale * ((in_val >> 4) & 0x01);
-            cur[4] = scale * ((in_val >> 3) & 0x01);
-            cur[5] = scale * ((in_val >> 2) & 0x01);
-            cur[6] = scale * ((in_val >> 1) & 0x01);
-            cur[7] = scale * ((in_val) & 0x01);
-        });
+        (&mut out_iter)
+            .zip(&mut in_iter)
+            .for_each(|(out_vals, in_val)| {
+                // make sure we only perform the bounds check once
+                let cur: &mut [u8; 8] = out_vals.try_into().unwrap();
+                // perform the actual expansion
+                cur[0] = scale * ((in_val >> 7) & 0x01);
+                cur[1] = scale * ((in_val >> 6) & 0x01);
+                cur[2] = scale * ((in_val >> 5) & 0x01);
+                cur[3] = scale * ((in_val >> 4) & 0x01);
+                cur[4] = scale * ((in_val >> 3) & 0x01);
+                cur[5] = scale * ((in_val >> 2) & 0x01);
+                cur[6] = scale * ((in_val >> 1) & 0x01);
+                cur[7] = scale * ((in_val) & 0x01);
+            });
 
         // handle the remainder at the end where the output is less than 8 bytes long
-        if let Some(in_val) = in_iter.next() {
+        if let Some(in_val) = in_iter.next()
+        {
             let remainder_iter = out_iter.into_remainder().iter_mut();
             remainder_iter.enumerate().for_each(|(pos, out_val)| {
                 let shift = (7_usize).wrapping_sub(pos);
@@ -218,17 +225,20 @@ pub(crate) fn expand_bits_to_byte(
         let mut out_iter = out.chunks_exact_mut(4);
 
         // same as above but adjusted to expand into 4 bytes instead of 8
-        (&mut out_iter).zip(&mut in_iter).for_each(|(out_vals, in_val)| {
-            let cur: &mut [u8; 4] = out_vals.try_into().unwrap();
+        (&mut out_iter)
+            .zip(&mut in_iter)
+            .for_each(|(out_vals, in_val)| {
+                let cur: &mut [u8; 4] = out_vals.try_into().unwrap();
 
-            cur[0] = scale * ((in_val >> 6) & 0x03);
-            cur[1] = scale * ((in_val >> 4) & 0x03);
-            cur[2] = scale * ((in_val >> 2) & 0x03);
-            cur[3] = scale * ((in_val) & 0x03);
-        });
+                cur[0] = scale * ((in_val >> 6) & 0x03);
+                cur[1] = scale * ((in_val >> 4) & 0x03);
+                cur[2] = scale * ((in_val >> 2) & 0x03);
+                cur[3] = scale * ((in_val) & 0x03);
+            });
 
         // handle the remainder at the end where the output is less than 4 bytes long
-        if let Some(in_val) = in_iter.next() {
+        if let Some(in_val) = in_iter.next()
+        {
             let remainder_iter = out_iter.into_remainder().iter_mut();
             remainder_iter.enumerate().for_each(|(pos, out_val)| {
                 let shift = (6_usize).wrapping_sub(pos * 2);
@@ -238,20 +248,22 @@ pub(crate) fn expand_bits_to_byte(
     }
     else if depth == 4
     {
-
         let mut in_iter = input.iter();
         let mut out_iter = out.chunks_exact_mut(2);
 
         // same as above but adjusted to expand into 2 bytes instead of 8
-        (&mut out_iter).zip(&mut in_iter).for_each(|(out_vals, in_val)| {
-            let cur: &mut [u8; 2] = out_vals.try_into().unwrap();
+        (&mut out_iter)
+            .zip(&mut in_iter)
+            .for_each(|(out_vals, in_val)| {
+                let cur: &mut [u8; 2] = out_vals.try_into().unwrap();
 
-            cur[0] = scale * ((in_val >> 4) & 0x0f);
-            cur[1] = scale * ((in_val) & 0x0f);
-        });
+                cur[0] = scale * ((in_val >> 4) & 0x0f);
+                cur[1] = scale * ((in_val) & 0x0f);
+            });
 
         // handle the remainder at the end
-        if let Some(in_val) = in_iter.next() {
+        if let Some(in_val) = in_iter.next()
+        {
             let remainder_iter = out_iter.into_remainder().iter_mut();
             remainder_iter.enumerate().for_each(|(pos, out_val)| {
                 let shift = (4_usize).wrapping_sub(pos * 4);
