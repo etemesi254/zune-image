2ad3a59eadc5b03297604d5b9bf6fc123a8f3ce4
caleb <etemesicaleb@gmail.com>
2023-04-09 12:38:31 +0300

    imageprocs/mathops: Return `u128` instead of `u64` to allow for division by 1.
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- crates/zune-imageprocs/src/mathops.rs
+++ crates/zune-imageprocs/src/mathops.rs
@@ -7,23 +7,22 @@
 /// Software: Practice and Experience  49 (6), 2019.
 #[inline(always)]
 #[must_use]
-pub fn compute_mod_u32(d: u64) -> u64
+pub fn compute_mod_u32(d: u64) -> u128
 {
     // operator precedence will be the end of me,,
-    return ((0xFFFF_FFFF_FFFF_FFFF_u64) / d) + 1;
+    return (u128::from(0xFFFF_FFFF_FFFF_FFFF_u64) / u128::from(d)) + 1;
 }
 
-/// Implement fast integer division from
-/// Daniel's Lemire fastmod.
-///  
-/// See [Faster Remainder by Direct Computation: Applications to Compilers and Software Libraries](https://arxiv.org/abs/1902.01961),
-/// Software: Practice and Experience  49 (6), 2019.
-#[inline(always)]
-#[must_use]
-pub fn mul128_u32(low_bits: u64, d: u32) -> u64
-{
-    return ((u128::from(low_bits) * u128::from(d)) >> 64) as u64;
-}
+// /// Implement fast integer division from
+// /// Daniel's Lemire fastmod.
+// ///
+// /// See [Faster Remainder by Direct Computation: Applications to Compilers and Software Libraries](https://arxiv.org/abs/1902.01961),
+// /// Software: Practice and Experience  49 (6), 2019.
+// #[inline(always)]
+// #[must_use]
+// fn mul128_u32(low_bits: u64, d: u32) -> u64 {
+//     return ((u128::from(low_bits) * u128::from(d)) >> 64) as u64;
+// }
 
 /// Fast division of u32 numbers
 ///  
@@ -33,7 +32,20 @@ pub fn mul128_u32(low_bits: u64, d: u32) -> u64
 #[inline(always)]
 #[allow(clippy::cast_possible_truncation)]
 #[must_use]
-pub fn fastdiv_u32(a: u32, m: u64) -> u32
+pub fn fastdiv_u32(a: u32, m: u128) -> u32
 {
-    mul128_u32(m, a) as u32
+    return ((m * u128::from(a)) >> 64) as u32;
+}
+
+/// Test fast_div works
+#[test]
+fn test_u8_div()
+{
+    for i in 1..1000
+    {
+        let num = compute_mod_u32(i);
+        let divisor = fastdiv_u32(13459, num);
+
+        assert_eq!(13459 / i, u64::from(divisor));
+    }
 }
