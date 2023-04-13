113cb00d7d0a6213636ac7fb159b0298f5ed74c7
caleb <etemesicaleb@gmail.com>
2023-04-13 16:34:43 +0300

    image/impls/rotate: Initial commit for rotate
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- zune-imageprocs/src/rotate.rs
+++ zune-imageprocs/src/rotate.rs
@@ -1,20 +1,38 @@
 use crate::flip::flip;
 
-pub fn rotate(angle: u16, in_image: &[u8], out_image: &mut [u8])
+pub fn rotate<T: Copy>(angle: f32, width: usize, in_image: &[T], out_image: &mut [T])
 {
-    let angle = angle % 360;
-    if angle == 180
+    let angle = angle % 360.0;
+
+    if angle == 180.0
     {
-        rotate_180(in_image, out_image);
+        // copy in image to out image
+        out_image.copy_from_slice(in_image);
+        rotate_180(out_image, width);
     }
 }
-fn rotate_180(in_image: &[u8], out_image: &mut [u8])
+
+/// Rotate an image by 180 degrees in place.
+///
+/// This method is preferred as it does it in place as opposed
+/// to the generic rotate which does it out of place
+pub fn rotate_180<T: Copy>(in_out_image: &mut [T], width: usize)
 {
-    // rotate 180 is the same as flip, so use that
-    // copy to dest
-    out_image.copy_from_slice(in_image);
-    // flip that.
-    flip(out_image);
+    // swap bottom row with top row
+
+    // divide array into two
+    let half = in_out_image.len() / 2;
+    let (top, bottom) = in_out_image.split_at_mut(half);
+
+    for (top_chunk, bottom_chunk) in top
+        .chunks_exact_mut(width)
+        .zip(bottom.chunks_exact_mut(width).rev())
+    {
+        for (a, b) in top_chunk.iter_mut().zip(bottom_chunk.iter_mut())
+        {
+            core::mem::swap(a, b)
+        }
+    }
 }
 
 fn _rotate_90(_in_image: &[u8], _out_image: &mut [u8], _width: usize, _height: usize)
