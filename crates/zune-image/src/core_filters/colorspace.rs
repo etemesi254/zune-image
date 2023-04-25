0a70391f4faddf1ae15c6df9f358cac459f480b0
caleb <etemesicaleb@gmail.com>
2023-04-25 23:14:34 +0300

    image/core_filters/colorspace: Add colorspace into core filters
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- crates/zune-image/src/core_filters/colorspace.rs
+++ crates/zune-image/src/core_filters/colorspace.rs
@@ -1,11 +1,14 @@
+use log::warn;
 use zune_core::bit_depth::BitType;
 use zune_core::colorspace::ColorSpace;
 
 use crate::channel::Channel;
+use crate::core_filters::colorspace::grayscale::{rgb_to_grayscale_u16, rgb_to_grayscale_u8};
 use crate::errors::ImageErrors;
-use crate::filters::grayscale::RgbToGrayScale;
 use crate::image::Image;
 use crate::traits::OperationsTrait;
+
+mod grayscale;
 
 pub struct ColorspaceConv
 {
@@ -70,6 +73,86 @@ fn convert_rgb_to_rgba(image: &mut Image) -> Result<(), ImageErrors>
     Ok(())
 }
 
+fn rgb_to_grayscale(image: &mut Image, preserve_alpha: bool) -> Result<(), ImageErrors>
+{
+    let im_colorspace = image.get_colorspace();
+
+    if im_colorspace == ColorSpace::Luma || im_colorspace == ColorSpace::LumaA
+    {
+        warn!("Image already in grayscale skipping this operation");
+        return Ok(());
+    }
+
+    let (width, height) = image.get_dimensions();
+    let size = width * height * image.get_depth().size_of();
+
+    let colorspace = image.get_colorspace();
+    let depth = image.get_depth();
+    let max_value = image.get_depth().max_value();
+
+    let mut out_colorspace = ColorSpace::Unknown;
+
+    for frame in image.get_frames_mut()
+    {
+        let channel = frame.get_channels_ref(colorspace, preserve_alpha);
+
+        match depth.bit_type()
+        {
+            BitType::U8 =>
+            {
+                let r = channel[0].reinterpret_as::<u8>().unwrap();
+                let g = channel[1].reinterpret_as::<u8>().unwrap();
+                let b = channel[2].reinterpret_as::<u8>().unwrap();
+                let mut out = Channel::new_with_length::<u8>(size);
+
+                rgb_to_grayscale_u8(
+                    r,
+                    g,
+                    b,
+                    out.reinterpret_as_mut::<u8>().unwrap(),
+                    max_value as u8
+                );
+
+                if preserve_alpha && colorspace.has_alpha()
+                {
+                    frame.set_channels(vec![out, channel[3].clone()]);
+                    out_colorspace = ColorSpace::LumaA;
+                }
+                else
+                {
+                    frame.set_channels(vec![out]);
+                    out_colorspace = ColorSpace::Luma;
+                }
+            }
+            BitType::U16 =>
+            {
+                let r = channel[0].reinterpret_as::<u16>().unwrap();
+                let g = channel[1].reinterpret_as::<u16>().unwrap();
+                let b = channel[2].reinterpret_as::<u16>().unwrap();
+                let mut out = Channel::new_with_length::<u16>(size);
+
+                rgb_to_grayscale_u16(r, g, b, out.reinterpret_as_mut::<u16>().unwrap(), max_value);
+
+                if preserve_alpha && colorspace.has_alpha()
+                {
+                    frame.set_channels(vec![out, channel[3].clone()]);
+                    out_colorspace = ColorSpace::LumaA;
+                }
+                else
+                {
+                    frame.set_channels(vec![out]);
+                    out_colorspace = ColorSpace::Luma;
+                }
+            }
+            _ => todo!()
+        }
+    }
+
+    assert_ne!(out_colorspace, ColorSpace::Unknown);
+
+    Ok(())
+}
+
 fn convert_rgb_bgr(from: ColorSpace, to: ColorSpace, image: &mut Image) -> Result<(), ImageErrors>
 {
     for frame in image.get_frames_mut()
@@ -128,8 +211,7 @@ impl OperationsTrait for ColorspaceConv
             (ColorSpace::RGB | ColorSpace::RGBA, ColorSpace::Luma | ColorSpace::LumaA) =>
             {
                 // use the rgb to grayscale converter
-                let converter = RgbToGrayScale::new().preserve_alpha(self.to.has_alpha());
-                converter.execute(image).unwrap();
+                rgb_to_grayscale(image, self.to.has_alpha())?;
             }
             (ColorSpace::Luma | ColorSpace::LumaA, ColorSpace::RGB | ColorSpace::RGBA) =>
             {
