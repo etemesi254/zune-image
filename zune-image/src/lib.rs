f2889d6b26476c9fb0d0065333e8b6a0e0f70659
caleb <etemesicaleb@gmail.com>
2023-04-27 00:51:13 +0300

    image/lib: Add filters documentation
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- zune-image/src/lib.rs
+++ zune-image/src/lib.rs
@@ -1,8 +1,44 @@
+/*
+ * Copyright (c) 2023.
+ *
+ * This software is free software; You can redistribute it or modify it under terms of the MIT, Apache License or Zlib license
+ */
+
 //! A fast and simple image processing library
 //!
-//! This ties up most of the independent crates in
+//! This ties up most of the independent crates and provides functionality
+//! between each one
 //!
 //!
+//! ## Features
+//! - The library crates include features for various formats anf filters
+//! - Decoders and encoders can be included or excluded at will
+//!
+//! ### Image decoders and encoders
+//! By default, a feature includes both format decoder and encoder if present.
+//!
+//! | Feature      | Decoder       | Encoder        |
+//! |--------------|---------------|----------------|
+//! | jpeg         | zune-jpeg     | [jpeg-encoder] |
+//! | png          | zune-png      | zune-png       |
+//! | ppm          | zune-ppm      | zune-ppm       |
+//! | qoi          | zune-qoi      | zune-qoi       |
+//! | farbfeld     | zune-farbfeld | zune-farbfeld  |
+//! | psd          | zune-psd      | -              |
+//! | jpeg-xl      | -             | zune-jpegxl    |
+//! | hdr          | zune-hdr      | zune-hdr       |
+//!
+//!
+//! ### Image filters
+//!
+//! Image filters are divided into two types,
+//!  - core filters: Things needed to enable conversions from one format to another.
+//!                 This may include color conversion and depth conversion routines.
+//!                 These are in `zune-image` crate
+//!  - extra filters: This include algorithms that do more complex pixel manipulations,
+//!     including contrast adjustment, resizing, blurring etc, the algorithms are usually
+//!     implemented in `zune-imageprocs` and the glue between algorithms and image is done in image
+//!     [filters](crate::filters)
 //!
 //! # Things the library is not good at.
 //!
@@ -16,16 +52,18 @@
 //! fit for your needs
 //!
 //![image]:https://crates.io/crates/image
+//! [jpeg-encoder]: https://crates.io/crates/jpeg-encoder
 #![allow(clippy::redundant_field_names, clippy::uninlined_format_args)]
 extern crate core;
 
 pub mod channel;
 pub mod codecs;
+pub mod core_filters;
 pub mod deinterleave;
 pub mod errors;
+pub mod filters;
 pub mod frame;
 pub mod image;
-pub mod impls;
 pub mod metadata;
 mod ops;
 mod serde;
