2fdb008d3fcd6e98d9083c827237400e17c9e7f4
caleb <etemesicaleb@gmail.com>
2023-04-27 00:46:31 +0300

    image/core_filters: Add colorspace and depth
    Signed-off-by: caleb <etemesicaleb@gmail.com>

---
+++ zune-image/src/core_filters.rs
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2023.
+ *
+ * This software is free software; You can redistribute it or modify it under terms of the MIT, Apache License or Zlib license
+ */
+
+//! Filters that cannot be disabled
+//!
+//! Core filters are filters needed for running of the
+pub mod colorspace;
+pub mod depth;
