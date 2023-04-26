db1c81894159b0d31978459f89283f4c1c8045ca
caleb <etemesicaleb@gmail.com>
2023-04-27 00:36:46 +0300

    image/core_filters/depth: Move depth conversion to core_filters
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- zune-image/src/codecs/png.rs
+++ zune-image/src/codecs/png.rs
@@ -1,17 +1,21 @@
 #![cfg(feature = "png")]
+#![allow(unused_variables)]
+
 //! Represents an png image decoder
+use exif::experimental::Writer;
 use log::{debug, info};
 use zune_core::bit_depth::BitDepth;
 use zune_core::colorspace::ColorSpace;
+use zune_core::options::EncoderOptions;
 use zune_core::result::DecodingResult;
 pub use zune_png::*;
 
-use crate::codecs::ImageFormat;
+use crate::codecs::{create_options_for_encoder, ImageFormat};
 use crate::deinterleave::{deinterleave_u16, deinterleave_u8};
 use crate::errors::ImageErrors;
 use crate::image::Image;
 use crate::metadata::ImageMetadata;
-use crate::traits::DecoderTrait;
+use crate::traits::{DecoderTrait, EncoderTrait};
 
 impl<'a> DecoderTrait<'a> for PngDecoder<'a>
 {
@@ -91,5 +95,101 @@ impl From<zune_png::error::PngDecodeErrors> for ImageErrors
         let err = format!("png: {from:?}");
 
         ImageErrors::ImageDecodeErrors(err)
+    }
+}
+
+#[derive(Default)]
+pub struct PngEncoder
+{
+    options: Option<EncoderOptions>
+}
+
+impl PngEncoder
+{
+    pub fn new() -> PngEncoder
+    {
+        PngEncoder::default()
+    }
+    pub fn new_with_options(options: EncoderOptions) -> PngEncoder
+    {
+        PngEncoder {
+            options: Some(options)
+        }
+    }
+}
+
+impl EncoderTrait for PngEncoder
+{
+    fn get_name(&self) -> &'static str
+    {
+        "PNG encoder"
+    }
+
+    fn encode_inner(&mut self, image: &Image) -> Result<Vec<u8>, ImageErrors>
+    {
+        let options = create_options_for_encoder(self.options, image);
+
+        let frame = &image.to_u8_be()[0];
+
+        let mut encoder = zune_png::PngEncoder::new(frame, options);
+
+        let mut buf = std::io::Cursor::new(vec![]);
+
+        #[cfg(feature = "metadata")]
+        {
+            use exif::experimental::Writer;
+
+            if !options.strip_metadata()
+            {
+                if let Some(fields) = &image.metadata.exif
+                {
+                    let mut writer = Writer::new();
+
+                    for metadatum in fields
+                    {
+                        writer.push_field(metadatum);
+                    }
+                    let result = writer.write(&mut buf, false);
+                    if result.is_ok()
+                    {
+                        encoder.add_exif_segment(buf.get_ref());
+                    }
+                    else
+                    {
+                        log::warn!("Writing exif failed {:?}", result);
+                    }
+                }
+            }
+        }
+        Ok(encoder.encode())
+    }
+
+    fn supported_colorspaces(&self) -> &'static [ColorSpace]
+    {
+        &[
+            ColorSpace::Luma,
+            ColorSpace::LumaA,
+            ColorSpace::RGB,
+            ColorSpace::RGBA
+        ]
+    }
+
+    fn format(&self) -> ImageFormat
+    {
+        ImageFormat::PNG
+    }
+
+    fn supported_bit_depth(&self) -> &'static [BitDepth]
+    {
+        &[BitDepth::Eight, BitDepth::Sixteen]
+    }
+
+    fn default_depth(&self, depth: BitDepth) -> BitDepth
+    {
+        match depth
+        {
+            BitDepth::Sixteen | BitDepth::Float32 => BitDepth::Sixteen,
+            _ => BitDepth::Eight
+        }
     }
 }
