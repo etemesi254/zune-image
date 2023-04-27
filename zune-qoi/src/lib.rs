e41f6c9d25d0124ffaa7656cbd879b49795ce4cf
caleb <etemesicaleb@gmail.com>
2023-04-28 01:08:16 +0300

    qoi: Support running in a no_std environment
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- zune-qoi/src/lib.rs
+++ zune-qoi/src/lib.rs
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2023.
+ *
+ * This software is free software; You can redistribute it or modify it under terms of the MIT, Apache License or Zlib license
+ */
+
+#![no_std]
+#![macro_use]
+extern crate alloc;
+
 pub use decoder::*;
 pub use encoder::*;
 pub use errors::*;
