4d29c0e67e4f829ec376eb8627e3f536bd829311
caleb <etemesicaleb@gmail.com>
2023-04-30 15:48:58 +0300

    png: Make a new release
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- crates/zune-png/src/utils/sse.rs
+++ crates/zune-png/src/utils/sse.rs
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2023.
+ *
+ * This software is free software; You can redistribute it or modify it under terms of the MIT, Apache License or Zlib license
+ */
+
 #![cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 //! SSE optimized utility functions
 
