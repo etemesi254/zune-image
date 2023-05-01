48f2c42062f809629df869053a6f4aa06f3467bf
caleb <etemesicaleb@gmail.com>
2023-05-02 00:58:06 +0300

    image/codecs: Add Bmp decoding
    Signed-off-by: caleb <etemesicaleb@gmail.com>

---
+++ zune-image/src/codecs/bmp.rs
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2023.
+ *
+ * This software is free software;
+ *
+ * You can redistribute it or modify it under terms of the MIT, Apache License or Zlib license
+ */
+#![cfg(feature = "bmp")]
+
+use zune_bmp::{BmpDecoder, BmpDecoderErrors};
+use zune_core::colorspace::ColorSpace;
+
+use crate::errors::ImageErrors;
+use crate::image::Image;
+use crate::traits::DecoderTrait;
+
+impl<'a> DecoderTrait<'a> for BmpDecoder<'a>
+{
+    fn decode(&mut self) -> Result<Image, ImageErrors>
+    {
+        let pixels = self.decode()?;
+        let (width, height) = self.get_dimensions().unwrap();
+        let colorspace = self.get_colorspace().unwrap();
+
+        Ok(Image::from_u8(&pixels, width, height, colorspace))
+    }
+
+    fn get_dimensions(&self) -> Option<(usize, usize)>
+    {
+        self.get_dimensions()
+    }
+
+    fn get_out_colorspace(&self) -> ColorSpace
+    {
+        self.get_colorspace().unwrap()
+    }
+
+    fn get_name(&self) -> &'static str
+    {
+        "BMP Decoder"
+    }
+}
+
+impl From<BmpDecoderErrors> for ImageErrors
+{
+    fn from(value: BmpDecoderErrors) -> Self
+    {
+        Self::ImageDecodeErrors(format!("bmp: {:?}", value))
+    }
+}
