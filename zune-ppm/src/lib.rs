9712a30b5b23581326024597ec3b565a5c228e72
caleb <etemesicaleb@gmail.com>
2023-05-02 00:56:31 +0300

    ppm: Make ppm no_std
    Signed-off-by: caleb <etemesicaleb@gmail.com>

--- zune-ppm/src/lib.rs
+++ zune-ppm/src/lib.rs
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2023.
+ *
+ * This software is free software;
+ *
+ * You can redistribute it or modify it under terms of the MIT, Apache License or Zlib license
+ */
+
+#![no_std]
+extern crate alloc;
+
 pub use crate::decoder::*;
 pub use crate::encoder::*;
 
