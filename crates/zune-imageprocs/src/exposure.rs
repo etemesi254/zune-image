04c894384b7794505ae13a95dd85d05a898926a4
caleb <etemesicaleb@gmail.com>
2023-12-23 19:40:28 +0300

    crates/exposure: Set black_point to scale based on depth

--- crates/zune-imageprocs/src/exposure.rs
+++ crates/zune-imageprocs/src/exposure.rs
@@ -17,10 +17,6 @@
 //! ```
 //!
 //! # Gotchas
-//! - `black`  argument is depth sensitive ,
-//!    -u8: range should be 0..255
-//!    - u16: range should be 0..65535
-//!    - f32: range should be between 0.0..1.0
 //! -`f32` depth doesn't do any clamping, hence values may get out of range
 use zune_core::bit_depth::BitType;
 use zune_image::errors::ImageErrors;
@@ -64,10 +60,11 @@ impl Exposure {
     ///  - exposure: Set the exposure correction,
     ///     Common range is from -3.0 to 3.0. Default should be zero
     ///
-    /// - black: Set black level correction,argument is depth sensitive ,
-    ///    - `u8`: range should be 0..255
-    ///    - `u16`: range should be 0..65535
-    ///    - `f32`: range should be between 0.0..1.0
+    /// - black: Set black level correction,should be between 0.0 and 1.0,
+    /// it will be scaled appropriately depending on image depth. E.g
+    /// for a depth of 8 it will be multiplied by 255, and for a depth of 16, by
+    /// 65535
+    ///
     #[must_use]
     pub fn new(exposure: f32, black: f32) -> Exposure {
         Exposure { exposure, black }
@@ -87,25 +84,27 @@ impl OperationsTrait for Exposure {
     fn execute_impl(&self, image: &mut Image) -> Result<(), ImageErrors> {
         let bit_type = image.depth().bit_type();
 
+        let black = self.black.clamp(0.0, 1.0) * f32::from(image.depth().max_value());
+
         for channel in image.channels_mut(true) {
             match bit_type {
                 BitType::U8 => {
                     let raw_px = channel.reinterpret_as_mut::<u8>()?;
                     for x in raw_px.iter_mut() {
-                        *x = ((f32::from(*x) - self.black) * self.exposure).clamp(0., 255.0) as _;
+                        *x = ((f32::from(*x) - black) * self.exposure).clamp(0., 255.0) as _;
                     }
                 }
                 BitType::U16 => {
                     let raw_px = channel.reinterpret_as_mut::<u16>()?;
                     for x in raw_px.iter_mut() {
-                        *x = ((f32::from(*x) - self.black) * self.exposure).clamp(0., 65535.0) as _;
+                        *x = ((f32::from(*x) - black) * self.exposure).clamp(0., 65535.0) as _;
                     }
                 }
                 BitType::F32 => {
                     let raw_px = channel.reinterpret_as_mut::<f32>()?;
                     raw_px
                         .iter_mut()
-                        .for_each(|x| *x = (*x - self.black) * self.exposure);
+                        .for_each(|x| *x = (*x - black) * self.exposure);
                 }
                 d => return Err(ImageErrors::ImageOperationNotImplemented(self.name(), d))
             }
