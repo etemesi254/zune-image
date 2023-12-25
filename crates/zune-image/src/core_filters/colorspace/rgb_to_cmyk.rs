cea03b7bb4b530c3bdec0f0b890711421e2a5139
caleb <etemesicaleb@gmail.com>
2023-12-25 16:26:56 +0300

    crates/image/core_filters/colorspace/rgb_to_cmyk: Add 16 bit converters.
    They scale it to 0-1, convert and rescale back to original

--- crates/zune-image/src/core_filters/colorspace/rgb_to_cmyk.rs
+++ crates/zune-image/src/core_filters/colorspace/rgb_to_cmyk.rs
@@ -113,6 +113,22 @@ pub fn rgb_to_cmyk_f32(r_to_c: &mut [f32], g_to_m: &mut [f32], b_to_y: &mut [f32
         *k = result[3];
     }
 }
+pub fn rgb_to_cmyk_u16(r_to_c: &mut [u16], g_to_m: &mut [u16], b_to_y: &mut [u16], k: &mut [u16]) {
+    let v = f32::from(u16::MAX);
+    let inv = 1.0 / v;
+    for (((r_c, g_m), b_y), k) in r_to_c.iter_mut().zip(g_to_m).zip(b_to_y).zip(k) {
+        // scale it to be between 0 and 1
+        let result = rgb_to_cmyk_inner_f32(
+            f32::from(*r_c) * inv,
+            f32::from(*g_m) * inv,
+            f32::from(*b_y) * inv
+        );
+        *r_c = (result[0] * v) as u16;
+        *g_m = (result[1] * v) as u16;
+        *b_y = (result[2] * v) as u16;
+        *k = (result[3] * v) as u16;
+    }
+}
 #[inline(always)]
 fn cmyk_to_rgb_f32_inner(c: f32, m: f32, y: f32, k: f32) -> [f32; 3] {
     let k_inv = 1.0 / (k / 255.0 + 0.5);
@@ -140,10 +156,38 @@ pub fn cmyk_to_rgb_f32(c_to_r: &mut [f32], m_to_g: &mut [f32], y_to_b: &mut [f32
         let y = *y_b;
         let k = *k;
         let [r, g, b] = cmyk_to_rgb_f32_inner(c, m, y, k);
-        // use a fast u8 divider borrowed from stb
 
         *c_r = r;
         *m_g = g;
         *y_b = b;
+    }
+}
+
+/// Convert CMYK to RGB
+///
+/// This modifies array in place, in order to conserve memory
+///
+/// # Arguments
+/// - c_to_r: Contains the Cyan  pixels, after this, they will contain the red pixels
+/// - m_to_g: Contains the Magenta pixels, after this, they will contain the green pixels
+/// - y_to_b: Contains the Yellow pixels, after this, they will contain the blue pixels
+///- k : Contains the key or black pixels,    
+pub fn cmyk_to_rgb_u16(c_to_r: &mut [u16], m_to_g: &mut [u16], y_to_b: &mut [u16], k: &[u16]) {
+    let v = f32::from(u16::MAX);
+    let inv = 1.0 / v;
+
+    for (((c_r, m_g), y_b), k) in c_to_r.iter_mut().zip(m_to_g).zip(y_to_b).zip(k) {
+        // scale the values to be between 0 and 1
+        let c = f32::from(*c_r) * inv;
+        let m = f32::from(*m_g) * inv;
+        let y = f32::from(*y_b) * inv;
+        let k = f32::from(*k) * inv;
+
+        let [r, g, b] = cmyk_to_rgb_f32_inner(c, m, y, k);
+
+        // scale back to be between 0 - 65535
+        *c_r = (r * v) as u16;
+        *m_g = (g * v) as u16;
+        *y_b = (b * v) as u16;
     }
 }
