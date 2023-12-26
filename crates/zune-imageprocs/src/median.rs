ddc953356ec49d890db3249895ee14e27a737919
caleb <etemesicaleb@gmail.com>
2023-12-26 14:18:38 +0300

    crates/imageprocs/median: Add median filter

--- crates/zune-imageprocs/src/median.rs
+++ crates/zune-imageprocs/src/median.rs
@@ -5,8 +5,38 @@
  *
  * You can redistribute it or modify it under terms of the MIT, Apache License or Zlib license
  */
-#![allow(dead_code)]
-use std::fmt::Debug;
+
+//! Applies a median filter of given dimensions to an image. Each output pixel is the median
+//! of the pixels in a `(2 * radius + 1) * (2 * radius + 1)` kernel of pixels in the input image.
+//!
+//! Performs O(radius) operations per pixel
+//!
+//! # Algorithm
+//! A simple median function can be implemented by sorting items in a window and then picking the middle value,
+//! but then this has some not so good perfomance especially with large windows
+//! e.g if I were calculating a radius of 37, the window would be `((2*37)+1* (2*37)+1))` -> `75 * 75` -> `5625` values
+//! to sort on every window position, which no matter what computer, it's gonna be slow
+//!
+//!  But one thing to note is that what happens from one window to another is that we drop the rightmost values and add the
+//! leftmost values
+//!
+//! ```text
+//!┌─┬───────────┬─┐
+//!│ │           │ │
+//!│ │           │ │
+//!│ │           │ │
+//!│ │           │ │
+//!└─┴───────────┴─┘
+//! ▲             ▲
+//! │             │
+//!drop           add
+//! ```
+//! So we can maintain a histogram of values in our window, the histogram tells us the frequencies of values in our windows
+//! on moving from one window to another, we drop the leftmost values, and add the rightmost
+//!
+//! while this is still expensive, it is faster than sorting
+//!
+//!
 
 use zune_core::bit_depth::BitType;
 use zune_core::log::trace;
@@ -15,7 +45,8 @@ use zune_image::errors::ImageErrors;
 use zune_image::image::Image;
 use zune_image::traits::OperationsTrait;
 
-//use crate::spatial::spatial_mut;
+use crate::pad::{pad, PadMethod};
+use crate::utils::z_prefetch;
 
 /// Median returns a new image in which each pixel is the median of its neighbors.
 ///
@@ -51,15 +82,15 @@ impl OperationsTrait for Median {
                 let mut new_channel = Channel::new_with_bit_type(channel.len(), depth.bit_type());
 
                 match depth.bit_type() {
-                    BitType::U16 => median(
+                    BitType::U16 => median_u16(
                         channel.reinterpret_as::<u16>()?,
                         new_channel.reinterpret_as_mut::<u16>()?,
                         self.radius,
                         width,
                         height
                     ),
-                    BitType::U8 => median(
-                        channel.reinterpret_as::<u8>().unwrap(),
+                    BitType::U8 => median_u8(
+                        channel.reinterpret_as::<u8>()?,
                         new_channel.reinterpret_as_mut::<u8>()?,
                         self.radius,
                         width,
@@ -87,14 +118,14 @@ impl OperationsTrait for Median {
                             Channel::new_with_bit_type(channel.len(), depth.bit_type());
 
                         match depth.bit_type() {
-                            BitType::U16 => median(
+                            BitType::U16 => median_u16(
                                 channel.reinterpret_as::<u16>()?,
                                 new_channel.reinterpret_as_mut::<u16>()?,
                                 self.radius,
                                 width,
                                 height
                             ),
-                            BitType::U8 => median(
+                            BitType::U8 => median_u8(
                                 channel.reinterpret_as::<u8>()?,
                                 new_channel.reinterpret_as_mut::<u8>()?,
                                 self.radius,
@@ -125,20 +156,221 @@ impl OperationsTrait for Median {
         &[BitType::U8, BitType::U16]
     }
 }
+#[allow(clippy::cast_possible_truncation)]
+pub fn median_u16(
+    in_channel: &[u16], out_channel: &mut [u16], radius: usize, width: usize, height: usize
+) {
+    /*
+     * Okay rico, we run a tight shift here
+     *
+     * The implementation is simple, we use nested lambdas, something dope about them
+     * is that they can modify parameters of the parent function, which we want.
+     *
+     * spatial_mut has a fixed definition, in that it wants a function that just accepts a slice, but what about histograms
+     * that need to be preserved across iterations, yep why the need for lambdas
+     */
 
-pub fn find_median<T: Copy + Ord>(array: &mut [T]) -> T {
-    array.sort_unstable();
-    let middle = array.len() / 2;
+    // array containing our histogram for each median window
+    let mut histogram_arr = vec![0_u32; 65536];
+    // give it a fixed size for optimizer to remove bounds check
+    let histogram: &mut [u32; 65536] = histogram_arr.get_mut(..).unwrap().try_into().unwrap();
 
-    array[middle]
+    let radius_size = (2 * radius) + 1;
+
+    // vector containing items that will be dropped from the
+    // histogram in the next iteration
+    let mut to_be_dropped = vec![0; radius_size];
+    // the current position of our window, used for handling edges and
+    // knowing when we went to another row
+    let mut counter = 0;
+
+    let func = |array: &[u16]| -> u16 {
+        // the position of our median
+        let median_pos = array.len() / 2;
+
+        if (counter % width) == 0 {
+            // when we are starting a new row
+
+            // zero out histogram
+            histogram.fill(0);
+            // add everything
+            for c in array {
+                histogram[usize::from(*c)] += 1;
+            }
+        } else {
+            // drop items that fell off from previous run
+            for x in &to_be_dropped {
+                histogram[usize::from(*x)] -= 1;
+            }
+            // add the new window values added to the histogram
+            // these are the rightmost values
+            array.chunks_exact(radius_size).for_each(|v| {
+                let to_add = usize::from(*v.last().unwrap());
+                histogram[to_add] += 1;
+            });
+        }
+
+        // iterate through our histogram to find the median
+        let mut accum = 0;
+        let mut median = 0;
+
+        for (pos, v) in histogram.iter().enumerate() {
+            accum += *v;
+            if accum >= median_pos as u32 {
+                // we found the median
+                median = pos as u16;
+                break;
+            }
+        }
+        counter += 1;
+        // set up for our next loop
+        // add the things to be dropped in since they fell off our window in the to_be_dropped
+        // in the next iteration, they will be subtracted from the histogram
+
+        debug_assert!(array.chunks_exact(radius_size).len() == to_be_dropped.len());
+        array
+            .chunks_exact(radius_size)
+            .zip(to_be_dropped.iter_mut())
+            .for_each(|(x, v)| {
+                *v = x[0];
+            });
+        median
+    };
+
+    // pad input
+    let padded_input = pad(
+        in_channel,
+        width,
+        height,
+        radius,
+        radius,
+        PadMethod::Replicate
+    );
+    spatial_median(&padded_input, out_channel, radius, width, height, func);
+}
+#[allow(clippy::cast_possible_truncation)]
+pub fn median_u8(
+    in_channel: &[u8], out_channel: &mut [u8], radius: usize, width: usize, height: usize
+) {
+    // duplicated from above, but uses array instead of vec, and
+
+    // array containing our histogram for each median window
+    let mut histogram = [0_u32; 256];
+
+    let radius_size = (2 * radius) + 1;
+
+    // vector containing items that will be dropped from the
+    // histogram in the next iteration
+    let mut to_be_dropped = vec![0; radius_size];
+    // the current position of our window, used for handling edges and
+    // knowing when we went to another row
+    let mut counter = 0;
+
+    let func = |array: &[u8]| -> u8 {
+        // the position of our median
+        let median_pos = array.len() / 2;
+
+        if (counter % width) == 0 {
+            // when we are starting a new row
+
+            // zero out histogram
+            histogram.fill(0);
+            // add everything
+            for c in array {
+                histogram[usize::from(*c)] += 1;
+            }
+        } else {
+            // drop items that fell off from previous run
+            for x in &to_be_dropped {
+                histogram[usize::from(*x)] -= 1;
+            }
+            // add the new window values added to the histogram
+            // these are the rightmost values
+            array.chunks_exact(radius_size).for_each(|v| {
+                let to_add = usize::from(*v.last().unwrap());
+                histogram[to_add] += 1;
+            });
+        }
+
+        // iterate through our histogram to find the median
+        let mut accum = 0;
+        let mut median = 0;
+
+        for (pos, v) in histogram.iter().enumerate() {
+            accum += *v;
+            if accum >= median_pos as u32 {
+                // we found the median
+                median = pos as u8;
+                break;
+            }
+        }
+        counter += 1;
+        // set up for our next loop
+        // add the things to be dropped in since they fell off our window in the to_be_dropped
+        // in the next iteration, they will be subtracted from the histogram
+
+        debug_assert!(array.chunks_exact(radius_size).len() == to_be_dropped.len());
+        array
+            .chunks_exact(radius_size)
+            .zip(to_be_dropped.iter_mut())
+            .for_each(|(x, v)| {
+                *v = x[0];
+            });
+        median
+    };
+
+    // pad input
+    let padded_input = pad(
+        in_channel,
+        width,
+        height,
+        radius,
+        radius,
+        PadMethod::Replicate
+    );
+    spatial_median(&padded_input, out_channel, radius, width, height, func);
 }
 
-/// Median returns a new image in which each pixel is the median of its neighbors.
-/// The parameter radius corresponds to the radius of the neighbor area to be searched,
-/// for example a radius of R will result in a search window length of 2R+1 for each dimension.
-pub fn median<T: Copy + Ord + Default + Debug>(
-    _in_channel: &[T], _out_channel: &mut [T], _radius: usize, _width: usize, _height: usize
-) {
-    //panic!();
-    //spatial_mut(in_channel, out_channel, radius, width, height, find_median);
+pub fn spatial_median<T, F>(
+    in_channel: &[T], out_channel: &mut [T], radius: usize, width: usize, height: usize,
+    mut function: F
+) where
+    T: Default + Copy,
+    F: FnMut(&[T]) -> T
+{
+    let old_width = width;
+    let height = (radius * 2) + height;
+    let width = (radius * 2) + width;
+
+    assert_eq!(height * width, in_channel.len());
+
+    let radius_size = (2 * radius) + 1;
+
+    let radius_loop = radius_size >> 1;
+
+    let mut local_storage = vec![T::default(); radius_size * radius_size];
+
+    for y in radius_loop..height - radius_loop {
+        for x in radius_loop..width - radius_loop {
+            let iy = y - radius_loop;
+            let ix = x - radius_loop;
+
+            let mut i = 0;
+
+            for ky in 0..radius_size {
+                let iy_i = iy + ky;
+
+                z_prefetch(in_channel, (iy_i + 1) * width + ix);
+                let in_slice = &in_channel[(iy_i * width) + ix..(iy_i * width) + ix + radius_size];
+                local_storage[i..i + radius_size].copy_from_slice(in_slice);
+                z_prefetch(in_channel, (iy_i + 2) * width + ix);
+
+                i += radius_size;
+            }
+
+            let result = function(&local_storage);
+
+            out_channel[iy * old_width + ix] = result;
+        }
+    }
 }
