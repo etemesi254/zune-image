cdbe176f4d6d5544e840ed704a8e8efca9f0037d
caleb <etemesicaleb@gmail.com>
2024-02-18 22:22:42 +0300

    crates/core/bytestream:  Optimize readers.
    Some functions are called multiple times in hot loops. Optimize them as much as possible

--- crates/zune-core/src/bytestream/reader/std_readers.rs
+++ crates/zune-core/src/bytestream/reader/std_readers.rs
@@ -11,8 +11,45 @@ where
     T: AsRef<[u8]>
 {
     #[inline(always)]
+    fn read_byte_no_error(&mut self) -> u8 {
+        let byte = *self
+            .get_ref()
+            .as_ref()
+            .get(self.position() as usize)
+            .unwrap_or(&0);
+        self.set_position(self.position() + 1);
+        byte
+    }
+    #[inline(always)]
     fn read_exact_bytes(&mut self, buf: &mut [u8]) -> Result<(), ZByteIoError> {
         self.read_exact(buf).map_err(ZByteIoError::from)
+    }
+
+    #[inline(always)]
+    fn read_const_bytes<const N: usize>(&mut self, buf: &mut [u8; N]) -> Result<(), ZByteIoError> {
+        if self.position() as usize + N < self.get_ref().as_ref().len() {
+            // we are in bounds
+            let reference = self.get_ref().as_ref();
+            let position = self.position() as usize;
+            if let Some(buf_ref) = reference.get(position..position + N) {
+                buf.copy_from_slice(buf_ref);
+                self.set_position((position + N) as u64);
+                return Ok(());
+            }
+        }
+        Err(ZByteIoError::Generic("Cannot satisfy read"))
+    }
+
+    fn read_const_bytes_no_error<const N: usize>(&mut self, buf: &mut [u8; N]) {
+        if self.position() as usize + N < self.get_ref().as_ref().len() {
+            // we are in bounds
+            let reference = self.get_ref().as_ref();
+            let position = self.position() as usize;
+            if let Some(buf_ref) = reference.get(position..position + N) {
+                buf.copy_from_slice(buf_ref);
+                self.set_position((position + N) as u64);
+            }
+        }
     }
 
     #[inline(always)]
@@ -70,14 +107,32 @@ where
 }
 
 impl<T: io::Read + io::Seek> ZByteIoTrait for BufReader<T> {
+    #[inline(always)]
+    fn read_byte_no_error(&mut self) -> u8 {
+        let mut buf = [0];
+        let _ = self.read(&mut buf);
+        buf[0]
+    }
+    #[inline(always)]
     fn read_exact_bytes(&mut self, buf: &mut [u8]) -> Result<(), ZByteIoError> {
         self.read_exact(buf).map_err(ZByteIoError::from)
     }
 
+    #[inline]
+    fn read_const_bytes<const N: usize>(&mut self, buf: &mut [u8; N]) -> Result<(), ZByteIoError> {
+        self.read_exact_bytes(buf)
+    }
+
+    fn read_const_bytes_no_error<const N: usize>(&mut self, buf: &mut [u8; N]) {
+        let _ = self.read_const_bytes(buf);
+    }
+
+    #[inline(always)]
     fn read_bytes(&mut self, buf: &mut [u8]) -> Result<usize, ZByteIoError> {
         self.read(buf).map_err(ZByteIoError::from)
     }
 
+    #[inline(always)]
     fn peek_bytes(&mut self, buf: &mut [u8]) -> Result<usize, ZByteIoError> {
         // first read bytes to the buffer
         let bytes_read = self.read_bytes(buf)?;
@@ -88,8 +143,7 @@ impl<T: io::Read + io::Seek> ZByteIoTrait for BufReader<T> {
         Ok(bytes_read)
     }
 
-    #[inline]
-
+    #[inline(always)]
     fn peek_exact_bytes(&mut self, buf: &mut [u8]) -> Result<(), ZByteIoError> {
         // first read bytes to the buffer
         self.read_exact_bytes(buf)?;
@@ -100,16 +154,19 @@ impl<T: io::Read + io::Seek> ZByteIoTrait for BufReader<T> {
         Ok(())
     }
 
+    #[inline(always)]
     fn z_seek(&mut self, from: ZSeekFrom) -> Result<u64, ZByteIoError> {
         self.seek(from.to_std_seek()).map_err(ZByteIoError::from)
     }
 
+    #[inline(always)]
     fn is_eof(&mut self) -> Result<bool, ZByteIoError> {
         self.fill_buf()
             .map(|b| b.is_empty())
             .map_err(ZByteIoError::from)
     }
 
+    #[inline(always)]
     fn z_size(&mut self) -> Result<i64, ZByteIoError> {
         let old_pos = self.stream_position()?;
         let len = self.seek(SeekFrom::End(0))?;
@@ -127,10 +184,12 @@ impl<T: io::Read + io::Seek> ZByteIoTrait for BufReader<T> {
         "BufReader<T>"
     }
 
+    #[inline(always)]
     fn z_position(&mut self) -> Result<u64, ZByteIoError> {
         self.stream_position().map_err(ZByteIoError::from)
     }
 
+    #[inline(always)]
     fn read_remaining(&mut self, sink: &mut Vec<u8>) -> Result<usize, ZByteIoError> {
         self.read_to_end(sink).map_err(ZByteIoError::from)
     }
