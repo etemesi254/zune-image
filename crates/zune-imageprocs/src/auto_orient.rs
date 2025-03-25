125e3a4c2f2e6484aab0fd18e80b594ae5652544
etemesi <caleb@techplain.io>
2025-03-25 13:01:18 +0300

    crates/imageprocs/auto_orient: Auto orient on the first byte and handle duplicates correctly

--- crates/zune-imageprocs/src/auto_orient.rs
+++ crates/zune-imageprocs/src/auto_orient.rs
@@ -43,12 +43,14 @@ impl OperationsTrait for AutoOrient {
     fn execute_impl(&self, image: &mut Image) -> Result<(), ImageErrors> {
         // check if we have exif orientation metadata and transform it
         // to be this orientation
+
         #[cfg(feature = "exif")]
         {
+
             use exif::{Tag, Value};
 
             if let Some(data) = image.metadata().clone().exif() {
-                for field in data {
+                'rotator:for field in data {
                     // look for the orientation tag
                     if field.tag == Tag::Orientation {
                         match &field.value {
@@ -91,6 +93,7 @@ impl OperationsTrait for AutoOrient {
                                         );
                                     }
                                 }
+                                break 'rotator;
                             }
                             _ => {
                                 warn!("Invalid exif orientation type, ignoring it");
