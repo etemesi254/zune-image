258bc7d32f789498b8aa7c40a477f77458a520e8
etemesi <caleb@techplain.io>
2026-04-08 10:30:39 +0300

    heic: Ad benchmarking script

--- crates/zune-heic/benches/heif_bench.rs
+++ crates/zune-heic/benches/heif_bench.rs
@@ -1,27 +1,23 @@
 use std::hint::black_box;
-use std::io::Cursor;
-
-use criterion::{Criterion, criterion_group, criterion_main};
+use criterion::{criterion_group, criterion_main, Criterion};
 use libheif_rs::{ColorSpace, HeifContext, LibHeif, RgbChroma};
+use std::io::Cursor;
 use zune_core::bytestream::ZCursor;
-use zune_core::options::DecoderOptions;
 use zune_heic::HeifDecoder;
 
 fn bench_heif_decoding(c: &mut Criterion) {
     // Read the file once into memory so we are benchmarking CPU decoding speed,
     // not disk I/O performance.
-    let image_bytes = std::fs::read("fuzz-samples/shelf-christmas-decoration.heic")
-        .expect("Failed to read test.heic. Please ensure the file exists.");
+    let image_bytes = std::fs::read("fuzz-samples/IMG_4862.HEIC").expect("Failed to read test.heic. Please ensure the file exists.");
 
     let mut group = c.benchmark_group("HEIF Decoding");
 
     // -- zune-heif Benchmark --
-    group.bench_function("zune-heif (software decoder)", |b| {
+    group.bench_function("zune-heif", |b| {
         b.iter(|| {
             // Recreate the cursor for each iteration
             let data = ZCursor::new(black_box(image_bytes.as_slice()));
-            let options = DecoderOptions::new_fast().hvec_set_use_videotoolbox(false);
-            let mut decoder = HeifDecoder::new_with_options(data,options);
+            let mut decoder = HeifDecoder::new(data);
 
             decoder.decode_headers().unwrap();
             let _colorspace = decoder.colorspace().unwrap();
@@ -31,25 +27,6 @@ fn bench_heif_decoding(c: &mut Criterion) {
             black_box(decoded_data);
         });
     });
-    #[cfg(target_os = "macos")]
-    {
-        group.bench_function("zune-heif (hardware decoder)", |b| {
-            b.iter(|| {
-                // Recreate the cursor for each iteration
-                let data = ZCursor::new(black_box(image_bytes.as_slice()));
-                let options = DecoderOptions::new_fast().hvec_set_use_videotoolbox(true);
-
-                let mut decoder = HeifDecoder::new(data);
-
-                decoder.decode_headers().unwrap();
-                let _colorspace = decoder.colorspace().unwrap();
-
-                // Decode the actual pixel data and black_box the result
-                let decoded_data = decoder.decode().unwrap();
-                black_box(decoded_data);
-            });
-        });
-    }
 
     // -- libheif-rs Benchmark --
     group.bench_function("libheif", |b| {
@@ -60,15 +37,15 @@ fn bench_heif_decoding(c: &mut Criterion) {
             let ctx = HeifContext::read_from_bytes(black_box(image_bytes.as_slice())).unwrap();
             let handle = ctx.primary_image_handle().unwrap();
 
+            let image = lib_heif.decode(
+                &handle,
+                ColorSpace::Rgb(RgbChroma::Rgb),
+                None,
+            ).unwrap();
             // Decode to Interleaved RGB to match standard pixel data extraction
-            // Decode the image
-            let image = lib_heif
-                .decode(&handle, ColorSpace::Rgb(RgbChroma::Rgb), None)
-                .unwrap();
-            // Get "pixels"
-            let planes = image.planes();
-            let interleaved_plane = planes.interleaved.unwrap();
-            black_box(image);
+            let pixel = black_box(image.planes().interleaved.unwrap());
+
+            black_box(pixel);
         });
     });
 
