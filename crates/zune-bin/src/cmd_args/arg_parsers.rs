0c7dbb0cdd7318597e34a64be0f4754076fbdecf
etemesi <caleb@techplain.io>
2026-04-30 17:34:45 +0300

    bin: Remove icolorspace in favour of string parsing

--- crates/zune-bin/src/cmd_args/arg_parsers.rs
+++ crates/zune-bin/src/cmd_args/arg_parsers.rs
@@ -8,70 +8,9 @@
 
 use clap::builder::PossibleValue;
 use clap::ValueEnum;
-use zune_core::colorspace::ColorSpace;
 use zune_imageprocs::resize::ResizeMethod;
 
-#[derive(Copy, Clone, Debug)]
-#[allow(clippy::upper_case_acronyms)]
-pub enum IColorSpace {
-    RGB,
-    GRAYSCALE,
-    YCbCr,
-    RGBA,
-    Luma,
-    LumaA
-}
 
-impl IColorSpace {
-    #[allow(dead_code)]
-    pub const fn to_colorspace(self) -> ColorSpace {
-        match self {
-            IColorSpace::RGB => ColorSpace::RGB,
-            IColorSpace::GRAYSCALE => ColorSpace::Luma,
-            IColorSpace::YCbCr => ColorSpace::YCbCr,
-            IColorSpace::RGBA => ColorSpace::RGBA,
-            IColorSpace::Luma => ColorSpace::Luma,
-            IColorSpace::LumaA => ColorSpace::LumaA
-        }
-    }
-}
-
-impl ValueEnum for IColorSpace {
-    fn value_variants<'a>() -> &'a [Self] {
-        &[
-            Self::RGBA,
-            Self::RGB,
-            Self::YCbCr,
-            Self::GRAYSCALE,
-            Self::Luma,
-            Self::LumaA
-        ]
-    }
-
-    fn to_possible_value(&self) -> Option<PossibleValue> {
-        Some(match self {
-            Self::RGBA => PossibleValue::new("rgba"),
-            Self::RGB => PossibleValue::new("rgb"),
-            Self::YCbCr => PossibleValue::new("ycbcr"),
-            Self::GRAYSCALE => PossibleValue::new("grayscale"),
-            Self::LumaA => PossibleValue::new("lumaA"),
-            Self::Luma => PossibleValue::new("luma")
-        })
-    }
-}
-
-impl std::str::FromStr for IColorSpace {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        for variant in Self::value_variants() {
-            if variant.to_possible_value().unwrap().matches(s, false) {
-                return Ok(*variant);
-            }
-        }
-        Err(format!("Invalid variant: {s}"))
-    }
-}
 #[derive(Copy, Clone, Debug)]
 pub enum IResizeMethod {
     Lanczos,
