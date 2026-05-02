ec004377e48b946f0cd0b622488baa374f208632
etemesi <caleb@techplain.io>
2026-05-02 23:56:58 +0300

    imageprocs/mathops: Add modulos support

--- crates/zune-imageprocs/src/mathops.rs
+++ crates/zune-imageprocs/src/mathops.rs
@@ -15,7 +15,7 @@
 /// Software: Practice and Experience  49 (6), 2019.
 #[inline(always)]
 #[must_use]
-pub (crate) fn compute_mod_u32(d: u64) -> u128 {
+pub(crate) fn compute_mod_u32(d: u64) -> u128 {
     // operator precedence will be the end of me,,
     return (u128::from(0xFFFF_FFFF_FFFF_FFFF_u64) / u128::from(d)) + 1;
 }
@@ -42,6 +42,20 @@ pub (crate) fn compute_mod_u32(d: u64) -> u128 {
 pub fn fastdiv_u32(a: u32, m: u128) -> u32 {
     return ((m * u128::from(a)) >> 64) as u32;
 }
+/// Fast modulos of u32 numbers
+///
+/// Implement fast integer modulos from
+/// See [Faster Remainder by Direct Computation: Applications to Compilers and Software Libraries](https://arxiv.org/abs/1902.01961),
+/// Software: Practice and Experience  49 (6), 2019.
+#[inline(always)]
+#[must_use]
+pub fn fastmod_u32(a: u32, m: u128, d: u32) -> u32 {
+    // Step 1: low 64 bits of (m * a)
+    let lowbits = (m * u128::from(a)) as u64;
+
+    // Step 2: high 64 bits of (lowbits * d)
+    ((u128::from(lowbits) * u128::from(d)) >> 64) as u32
+}
 
 /// Test fast_div works
 #[test]
@@ -51,5 +65,19 @@ fn test_u8_div() {
         let divisor = fastdiv_u32(13459, num);
 
         assert_eq!(13459 / i, u64::from(divisor));
+    }
+}
+
+#[test]
+fn test_u8_mod() {
+    use nanorand::Rng;
+
+    for i in 1..1000 {
+        let rng = (u64::from_be_bytes(nanorand::WyRand::new().rand()) >> 32) as u32;
+
+        let num = compute_mod_u32(u64::from(rng));
+        let divisor = fastdiv_u32(rng, num);
+
+        assert_eq!(rng / i, divisor);
     }
 }
