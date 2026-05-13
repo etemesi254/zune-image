fd17074f3a18d7c0291cba89044fcf2531dfb934
etemesi <caleb@techplain.io>
2026-05-14 00:25:34 +0300

    png: Improve palette decoding and a bit better benchmarking framework

--- benchmarks/benches/decode_png.rs
+++ benchmarks/benches/decode_png.rs
@@ -9,16 +9,18 @@
 use std::fs::read;
 use std::hint::black_box;
 use std::io::Cursor;
+use std::path::Path;
 use std::time::Duration;
 
-use criterion::{ criterion_group, criterion_main, Criterion, Throughput};
-use spng::DecodeFlags;
+use criterion::{criterion_group, criterion_main, Criterion, Throughput};
+use spng::{ContextFlags, DecodeFlags};
 use zune_benches::sample_path;
 use zune_jpeg::zune_core::bytestream::ZCursor;
 
 fn decode_ref(data: &[u8]) -> Vec<u8> {
     let mut decoder = png::Decoder::new(Cursor::new(data));
     decoder.set_transformations(png::Transformations::EXPAND);
+    decoder.ignore_checksums(true);
 
     let mut reader = decoder.read_info().unwrap();
 
@@ -40,6 +42,7 @@ fn decode_spng(data: &[u8]) -> Vec<u8> {
     let cursor = std::io::Cursor::new(data);
     let mut decoder = spng::Decoder::new(cursor);
     decoder.set_decode_flags(DecodeFlags::TRANSPARENCY);
+    decoder.set_context_flags(ContextFlags::IGNORE_ADLER32);
 
     let (_, mut reader) = decoder.read_info().unwrap();
     let output_buffer_size = reader.output_buffer_size();
@@ -48,11 +51,10 @@ fn decode_spng(data: &[u8]) -> Vec<u8> {
     out
 }
 
-fn decode_test(c: &mut Criterion) {
-    let path = sample_path().join("test-images/png/benchmarks/speed_bench.png");
+fn generic_bench<S: AsRef<Path>>(c: &mut Criterion, path: S, name: &str) {
     let data = read(path).unwrap();
 
-    let mut group = c.benchmark_group("png: PNG decoding baseline");
+    let mut group = c.benchmark_group(name);
     group.throughput(Throughput::Bytes(data.len() as u64));
 
     group.bench_function("zune-png", |b| {
@@ -66,74 +68,52 @@ fn decode_test(c: &mut Criterion) {
     group.bench_function("spng", |b| {
         b.iter(|| black_box(decode_spng(data.as_slice())))
     });
+}
+
+fn decode_test(c: &mut Criterion) {
+    generic_bench(
+        c,
+        sample_path().join("test-images/png/benchmarks/speed_bench.png"),
+        "png: PNG decoding baseline",
+    );
 }
 
 fn decode_test_interlaced(c: &mut Criterion) {
-    let path = sample_path().join("test-images/png/benchmarks/speed_bench_interlaced.png");
-
-    let data = read(path).unwrap();
-
-    let mut group = c.benchmark_group("png: PNG decoding interlaced 8bpp");
-    group.throughput(Throughput::Bytes(data.len() as u64));
-
-    group.bench_function("zune-png", |b| {
-        b.iter(|| black_box(decode_zune(data.as_slice())))
-    });
-
-    group.bench_function("image-rs/png", |b| {
-        b.iter(|| black_box(decode_ref(data.as_slice())))
-    });
-
-    group.bench_function("spng", |b| {
-        b.iter(|| black_box(decode_spng(data.as_slice())))
-    });
+    generic_bench(
+        c,
+        sample_path().join("test-images/png/benchmarks/speed_bench_interlaced.png"),
+        "png: PNG decoding interlaced 8bpp",
+    );
 }
 
 fn decode_test_16_bit(c: &mut Criterion) {
-    let path = sample_path().join("test-images/png/benchmarks/speed_bench_16.png");
-    let data = read(path).unwrap();
-
-    let mut group = c.benchmark_group("png: PNG decoding  16 bpp");
-    group.throughput(Throughput::Bytes(data.len() as u64));
-
-    group.bench_function("zune-png", |b| {
-        b.iter(|| black_box(decode_zune(data.as_slice())))
-    });
-
-    group.bench_function("image-rs/png", |b| {
-        b.iter(|| black_box(decode_ref(data.as_slice())))
-    });
-
-    group.bench_function("spng", |b| {
-        b.iter(|| black_box(decode_spng(data.as_slice())))
-    });
+    generic_bench(
+        c,
+        sample_path().join("test-images/png/benchmarks/speed_bench_16.png"),
+        "png: PNG decoding  16 bpp"
+    );
 }
 
-fn decode_test_trns_chunk(c: &mut Criterion) {
-    let path = sample_path().join("test-images/png/benchmarks/speed_bench_palette.png");
-    let data = read(path).unwrap();
-
-    let mut group = c.benchmark_group("png: PNG decoding palette image");
-    group.throughput(Throughput::Bytes(data.len() as u64));
-
-    group.bench_function("zune-png", |b| {
-        b.iter(|| black_box(decode_zune(data.as_slice())))
-    });
-
-    group.bench_function("image-rs/png", |b| {
-        b.iter(|| black_box(decode_ref(data.as_slice())))
-    });
-
-    group.bench_function("spng", |b| {
-        b.iter(|| black_box(decode_spng(data.as_slice())))
-    });
+fn decode_test_palette(c: &mut Criterion) {
+    generic_bench(
+        c,
+        sample_path().join("test-images/png/benchmarks/speed_bench_palette.png"),
+        "png: PNG decoding palette image",
+    );
 }
+// fn decode_test_wallhaven(c: &mut Criterion) {
+//     generic_bench(
+//         c,
+//         "/Users/etemesi/Downloads/wallhaven-lywpjl_3840x2400.png",
+//         "png: PNG decoding wallhaven",
+//     );
+// }
 criterion_group!(name=benches;
   config={
   let c = Criterion::default();
     c.measurement_time(Duration::from_secs(20))
   };
-targets=decode_test_trns_chunk,decode_test_16_bit,decode_test,decode_test_interlaced
+targets=decode_test_palette,decode_test_16_bit,decode_test,decode_test_interlaced
 );
 
 criterion_main!(benches);
