8c42fbba64a7b58009bd122c7404383063c75403
etemesi <caleb@techplain.io>
2026-05-23 13:02:02 +0300

    imageprocs/blur: Fix some generic impl

--- crates/zune-imageprocs/src/blur/fast_gaussian_blur/x86_64.rs
+++ crates/zune-imageprocs/src/blur/fast_gaussian_blur/x86_64.rs
@@ -35,7 +35,7 @@ pub unsafe fn vertical_blur_region_u8_avx2(
     let start_y = (y_offset as isize) - (radius_isize * 2);
     let end_y = (y_offset + current_height) as isize;
     let mut ring_buffer_vec = vec![sim_zero; RING_SIZE];
-    let ring_buffer = as_mut_array(ring_buffer_vec.as_mut_slice()).unwrap();
+    let ring_buffer: &mut [_; N] = as_mut_array(ring_buffer_vec.as_mut_slice()).unwrap();
 
 
     for c in 0..region.src_channels.len() {
